78d2df5d682981cb36ff0555fc6b97b6bae079d1
delphinus35 <delphinus@remora.cx>
2013-03-26 14:49:25 +0900

    fix powerline segments

--- .config/powerline/ext/tmux/segments/custom.py
+++ .config/powerline/ext/tmux/segments/custom.py
@@ -6,6 +6,7 @@ from __future__ import absolute_import
 import json
 import sys
 import os
+import psutil
 
 class NowPlayingSegment(object):
 	STATE_SYMBOLS = {
@@ -47,8 +48,10 @@ class NowPlayingSegment(object):
 		now_playing_str = self._run_cmd([os.environ.get('MYPERL'),
 				os.path.expanduser('~/git/dotfiles/.screen/lastfm-json.pl')])
 		data = json.loads(now_playing_str)
+		state = 'fallback' if data.get('playing') == 'played' else 'play'
 		return {
-				'state': 'fallback' if data.get('playing') == 'played' else 'play',
+				'state': state,
+				'state_symbol': self.STATE_SYMBOLS[state],
 				'artist': data.get('artist'),
 				'title': data.get('title'),
 				'elapsed': data.get('timestamp'),
@@ -60,3 +63,13 @@ if __name__ == '__main__':
 		player = 'lastfm',
 		format = '{state_symbol} {artist} - {title} ({elapsed})',
 		))
+
+def cpu_load_percent_gradient(pl, format='{0}%', measure_interval=.5):
+	cpu_percent = int(psutil.cpu_percent(interval=measure_interval))
+	return [{
+			'contents': format.format(cpu_percent),
+			'highlight_goup': ['cpu_load_percent_gradient', 'cpu_load_percent'],
+			'draw_divider': True,
+			'divider_highlight_group': 'background:divider',
+			'gradient_level': cpu_percent,
+			}]
