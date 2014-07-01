f5ce372d7cbfc4a44265d54dca1145a27b90bfda
delphinus35 <delphinus@remora.cx>
2014-07-01 17:30:32 +0900

    peco-select-history

---
+++ .zsh/peco-select-history.zsh
@@ -0,0 +1,13 @@
+function peco-select-history() {
+    local tac
+    if which tac > /dev/null; then
+        tac="tac"
+    else
+        tac="tail -r"
+    fi
+    BUFFER=$(history -n 1 | eval $tac | peco --query "$LBUFFER")
+    CURSOR=$#BUFFER
+    zle clear-screen
+}
+zle -N peco-select-history
+bindkey '^r' peco-select-history
