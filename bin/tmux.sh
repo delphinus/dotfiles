ac851e1827f83f659859e5ae2f16812c4c01fbb2
delphinus <delphinus@remora.cx>
2014-08-26 14:09:08 +0900

    tmux.sh

--- bin/tmux.sh
+++ bin/tmux.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 TERM=xterm-256color
-tmux_cmd="tmux -u2 -f $HOME/git/dotfiles/.tmux.conf"
+tmux_cmd="tmux -u2 -f $HOME/.tmux.conf"
 if [ -z $TMUX ]; then
   if $(tmux has-session 2> /dev/null); then
     $tmux_cmd attach
