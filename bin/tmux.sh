af990fefea3ff269d795f5d774020e3a56a38c82
delphinus <delphinus@remora.cx>
2014-09-17 20:58:02 +0900

    tmux.sh for Mac

--- bin/tmux.sh
+++ bin/tmux.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 TERM=xterm-256color
-tmux_cmd="tmux -u2 -f $HOME/.tmux.conf"
+if [ "$tmux_cmd" == "" ]; then
+  tmux_cmd="tmux -u2 -f $HOME/.tmux.conf"
+fi
 if [ -z $TMUX ]; then
   if $(tmux has-session 2> /dev/null); then
     $tmux_cmd attach
