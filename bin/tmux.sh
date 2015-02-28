2d11b4fe147a975e25b66efc6fa9fd8e63dc016d
delphinus <delphinus@remora.cx>
2015-02-28 18:07:25 +0900

    select tmux prefix if ssh or not

--- bin/tmux.sh
+++ bin/tmux.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
+export POWERLINE_LIB=$(python -c 'import sys;import site;sys.stdout.write(site.USER_SITE)')/powerline
 TERM=xterm-256color
 if [ "$tmux_cmd" == "" ]; then
-  tmux_cmd="tmux -u2 -f $HOME/.tmux.conf" $@
+  tmux_cmd="tmux -u2 -f $HOME/.tmux.conf"
 fi
 if [ -z $TMUX ]; then
   if $(tmux has-session 2> /dev/null); then
-    $tmux_cmd attach
+    $tmux_cmd attach $@
   else
-    $tmux_cmd
+    $tmux_cmd $@
   fi
 fi
