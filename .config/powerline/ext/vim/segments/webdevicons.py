a2ce9d3f8235d4ca02a1880197675e4249989f9f
delphinus <delphinus@remora.cx>
2015-05-01 23:11:02 +0900

    webdevicons in powerline

---
+++ .config/powerline/ext/vim/segments/webdevicons.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# vim:se fenc=utf8 noet:
+from __future__ import (unicode_literals, division, absolute_import, print_function)
+
+try:
+	import vim
+except ImportError:
+	vim = {}
+
+from powerline.bindings.vim import vim_get_func
+
+def webdevicons(pl):
+	webdevicons = vim_get_func('WebDevIconsGetFileTypeSymbol')
+	return [] if not webdevicons else [{
+		'contents': webdevicons(),
+		'highlight_group': ['file_directory', 'file_name', 'webdevicons'],
+		}]
