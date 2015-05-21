ecd45d5f8843342d16068653a337a53420579100
delphinus <delphinus@remora.cx>
2015-05-21 14:35:07 +0900

    tmux command in tmux

--- bin/tmux.sh
+++ bin/tmux.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
+if [ -n "$TMUX" ]; then
+  exec tmux $@
+fi
+
 export POWERLINE_LIB=$(python -c 'import sys;import site;sys.stdout.write(site.USER_SITE)')/powerline
 TERM=xterm-256color
 if [ -z "$tmux_cmd" ]; then
   tmux_cmd="tmux -u2 -f $HOME/.tmux.conf"
 fi
-if [ -z $TMUX ]; then
-  if $(tmux has-session 2> /dev/null); then
-    $tmux_cmd attach $@
-  else
-    $tmux_cmd $@
-  fi
+if $(tmux has-session 2> /dev/null); then
+  $tmux_cmd attach $@
+else
+  $tmux_cmd $@
 fi
