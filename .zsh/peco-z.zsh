ff10fe0c99682a39887eb8285039bc7e6f737c80
delphinus <delphinus@remora.cx>
2015-05-23 17:24:04 +0900

    typo

--- .zsh/peco-z.zsh
+++ .zsh/peco-z.zsh
@@ -1,6 +1,6 @@
 function peco-z() {
   local selected_dir=${(@f)$(z | sort -k1nr \
-    | ruby -pe '$_.sub! /(?<=.{11})#{ENV["HOME"]}/, "~"' \
+    | ruby -pe '$_.sub! /(?<=^.{11})#{ENV["HOME"]}/, "~"' \
     | ruby -pe '$_.sub!(/^([\d.]+)\s+(.*)$/) { "%5d  %s" % [$1.to_i, $2] }' \
     | peco --prompt "CD HISTORY>" --query "$LBUFFER" \
     | cut -b 8- \
