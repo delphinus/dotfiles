f87f1c56ebda89d9975d8593f5f1253118c1b254
delphinus <delphinus@remora.cx>
2015-05-23 23:32:42 +0900

    peco-src-dir

--- .zsh/peco-ghq.zsh
+++ .zsh/peco-ghq.zsh
@@ -1,9 +1,20 @@
 function peco-src () {
-    local selected_dir=$(ghq list --full-path | peco --query "$LBUFFER")
-    if [ -n "$selected_dir" ]; then
-        BUFFER="cd ${selected_dir}"
-        zle accept-line
-    fi
+  local selected_dir=$(ghq list --full-path | peco --query "$LBUFFER")
+  if [ -n "$selected_dir" ]; then
+    BUFFER="cd ${selected_dir}"
+    zle accept-line
+  fi
 }
 zle -N peco-src
 bindkey '^]' peco-src
+
+function peco-src-dir () {
+  local current_buffer=$BUFFER
+  local selected_dir=$(ghq list --full-path | peco --query "$LBUFFER")
+  if [ -n "$selected_dir" ]; then
+    BUFFER="${current_buffer}${selected_dir}"
+    CURSOR=$#BUFFER
+  fi
+}
+zle -N peco-src-dir
+bindkey '^x^]' peco-src-dir
