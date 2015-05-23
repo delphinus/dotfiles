02a186f7308fc03f0537c097002e7b5d3ed82217
delphinus <delphinus@remora.cx>
2015-05-23 17:00:08 +0900

    move |

--- .zsh/peco-z.zsh
+++ .zsh/peco-z.zsh
@@ -3,12 +3,12 @@ function peco-z() {
     alias tac='tail -r'
     local aliased=1
   fi
-  local selected_dir=${(@f)$(z | tac | \
-    ruby -pe '$_.sub! /(?<=.{11})#{ENV["HOME"]}/, "~"' | \
-    ruby -pe '$_.sub!(/^([\d.]+)\s+(.*)$/) { "%5d  %s" % [$1.to_i, $2] }' | \
-    peco --prompt "CD HISTORY>" --query "$LBUFFER" | \
-    cut -b 8- | \
-    ruby -pe '$_.sub! /^~/, ENV["HOME"]' \
+  local selected_dir=${(@f)$(z | tac \
+    | ruby -pe '$_.sub! /(?<=.{11})#{ENV["HOME"]}/, "~"' \
+    | ruby -pe '$_.sub!(/^([\d.]+)\s+(.*)$/) { "%5d  %s" % [$1.to_i, $2] }' \
+    | peco --prompt "CD HISTORY>" --query "$LBUFFER" \
+    | cut -b 8- \
+    | ruby -pe '$_.sub! /^~/, ENV["HOME"]' \
     )}
   if [ "$aliased" = 1 ]; then
     unalias tac
