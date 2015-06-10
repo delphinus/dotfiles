e781c49a3929735193616383ae2dae7f47f579e5
delphinus <delphinus@remora.cx>
2015-06-10 23:19:30 +0900

    use system python for powerline

--- bin/tmux.sh
+++ bin/tmux.sh
@@ -3,7 +3,12 @@ if [ -n "$TMUX" ]; then
   exec tmux $@
 fi
 
-export POWERLINE_LIB=$(python -c 'import sys;import site;sys.stdout.write(site.USER_SITE)')/powerline
+if [ `uname` = 'Darwin' ]; then
+  python=/usr/bin/python
+else
+  python=python
+fi
+export POWERLINE_LIB=$($python -c 'import sys;import site;sys.stdout.write(site.USER_SITE)')/powerline
 TERM=xterm-256color
 if [ -z "$tmux_cmd" ]; then
   tmux_cmd="tmux -u2 -f $HOME/.tmux.conf"
