622fb8b8c582a56474523d3cc9ecd680c78a0c64
delphinus <delphinus@remora.cx>
2015-06-10 23:23:32 +0900

    peco-bundler

---
+++ .zsh/peco-bundler.zsh
@@ -0,0 +1,20 @@
+function peco-bundler () {
+  local selected_dir=$(bundler show --paths | peco --query "$LBUFFER")
+  if [ -n "$selected_dir" ]; then
+    BUFFER="cd ${selected_dir}"
+    zle accept-line
+  fi
+}
+zle -N peco-bundler
+bindkey '^g' peco-bundler
+
+function peco-bundler-dir () {
+  local current_buffer=$BUFFER
+  local selected_dir=$(bundler show --paths | peco --query)
+  if [ -n "$selected_dir" ]; then
+    BUFFER="${current_buffer}${selected_dir}"
+    CURSOR=$#BUFFER
+  fi
+}
+zle -N peco-bundler-dir
+bindkey '^x^g' peco-bundler-dir
