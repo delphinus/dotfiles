6271ddc347c7ce5863af37026c8ce9e13222d464
delphinus <delphinus@remora.cx>
2015-06-15 22:15:26 +0900

    brew directories

--- bin/brew-directories.rb
+++ bin/brew-directories.rb
@@ -4,6 +4,6 @@ prefix = Pathname `brew --prefix`.chomp
 puts(`brew ls --versions`.split("\n").each_with_object([]) do |str, ary|
   brew, *versions = str.split /\s+/
   versions.each do |v|
-    ary << prefix + 'Celler' + brew + v
+    ary << prefix + 'Cellar' + brew + v
   end
 end.join "\n")
