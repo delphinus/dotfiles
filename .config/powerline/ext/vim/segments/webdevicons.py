12ea1f0dbd17200f1b673675e42822bfa6c77338
delphinus <delphinus@remora.cx>
2015-06-23 23:58:42 +0900

    change webdevicons by buffers

--- .config/powerline/ext/vim/segments/webdevicons.py
+++ .config/powerline/ext/vim/segments/webdevicons.py
@@ -7,11 +7,14 @@ try:
 except ImportError:
 	vim = {}
 
-from powerline.bindings.vim import vim_get_func
+from powerline.bindings.vim import (vim_get_func, buffer_name)
+from powerline.theme import requires_segment_info
 
-def webdevicons(pl):
+@requires_segment_info
+def webdevicons(pl, segment_info):
 	webdevicons = vim_get_func('WebDevIconsGetFileTypeSymbol')
+	name = buffer_name(segment_info)
 	return [] if not webdevicons else [{
-		'contents': webdevicons(),
+		'contents': webdevicons(name),
 		'highlight_groups': ['webdevicons', 'file_name'],
 		}]
