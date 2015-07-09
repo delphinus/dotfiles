a4dbe323a8430f1657beec078b23cd1b5d6fbb8c
delphinus <delphinus@remora.cx>
2015-07-09 09:25:33 +0900

    use xterm-256color-italic

--- bin/tmux.sh
+++ bin/tmux.sh
@@ -9,7 +9,7 @@ else
   python=python
 fi
 export POWERLINE_LIB=$($python -c 'import sys;import site;sys.stdout.write(site.USER_SITE)')/powerline
-TERM=xterm-256color
+export TERM=xterm-256color-italic
 if [ -z "$tmux_cmd" ]; then
   tmux_cmd="tmux -u2 -f $HOME/.tmux.conf"
 fi
