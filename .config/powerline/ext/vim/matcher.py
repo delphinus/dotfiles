a5ecffa33404455a223184edd0adf3d1b76ca60c
delphinus <delphinus@remora.cx>
2016-01-09 15:10:11 +0900

    Fix encoding error in powerline

--- .config/powerline/ext/vim/matcher.py
+++ .config/powerline/ext/vim/matcher.py
@@ -17,18 +17,18 @@ def calc(matcher_info):
 	return vim_getbufoption(matcher_info, 'filetype') == 'vimcalc'
 
 def fugitive(matcher_info):
-	name = matcher_info['buffer'].name
+	name = matcher_info['buffer'].name.decode('utf-8')
 	return name and name.find('fugitive://') == 0
 
 def calendar(matcher_info):
-	name = matcher_info['buffer'].name
+	name = matcher_info['buffer'].name.decode('utf-8')
 	return name and os.path.basename(name).find('__Calendar__') == 0
 
 def vimshell(matcher_info):
 	return vim_getbufoption(matcher_info, 'filetype') == 'vimshell'
 
 def iexe_mysql(matcher_info):
-	name = matcher_info['buffer'].name
+	name = matcher_info['buffer'].name.decode('utf-8')
 	return name and os.path.basename(name).find('iexe-mysql') == 0
 
 def ref_perldoc(matcher_info):
