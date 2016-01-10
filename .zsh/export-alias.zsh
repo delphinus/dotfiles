2f6dba22986b0a6b6bfb55a672fa1ed9398e9f83
delphinus <delphinus@remora.cx>
2016-01-10 22:44:03 +0900

    Add alias for `npm run`

--- .zsh/export-alias.zsh
+++ .zsh/export-alias.zsh
@@ -36,6 +36,7 @@ alias l.="$LS --color -d .*"
 alias ll="$LS --color -l"
 alias ls="$LS --color"
 alias lv='lv -c'
+alias nr='npm run'
 alias ns='npm start'
 alias pp=prettyping
 alias stripcolors='sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})*)?[mGK]//g"'
