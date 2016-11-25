71922f5ca67aeaebb89125b9ba11485caec1fd5f
delphinus <delphinus@remora.cx>
2016-11-25 14:24:20 +0900

    Fix set-ssh-auth-sock.zsh

--- .zsh/set-ssh-auth-sock.zsh
+++ .zsh/set-ssh-auth-sock.zsh
@@ -1,11 +1,20 @@
 if [ -n "$SSH_AUTH_SOCK" ]; then
-  if [ -n "$SSH_CONNECTION" ]; then
-    if [ "$SSH_AUTH_SOCK" != "$HOME/.ssh/auth_sock" ]; then
-        ln -fs $SSH_AUTH_SOCK $HOME/.ssh/auth_sock
-    fi
-  elif ! [ -S "$SSH_AUTH_SOCK" ]; then
-    sock=$(ls /tmp/**/Listeners)
-    ln -fs $sock $SSH_AUTH_SOCK
+  if [ "$SSH_AUTH_SOCK" != "$HOME/.ssh/auth_sock" ]; then
+    ln -fs $SSH_AUTH_SOCK $HOME/.ssh/auth_sock
+    export SSH_AUTH_SOCK=$HOME/.ssh/auth_sock
   fi
-  export SSH_AUTH_SOCK=$HOME/.ssh/auth_sock
+  if ! [ -S "$SSH_AUTH_SOCK" ]; then
+    sock=$(ls /tmp/**/Listeners || true)
+    if [ -n "$sock" ]; then
+      ln -fs $sock $SSH_AUTH_SOCK
+    else
+      sock=$(ls /tmp/ssh*/agent* || true)
+      if [ -n "$sock" ]; then
+        ln -fs $sock $SSH_AUTH_SOCK
+      else
+        echo 'sock file cannot be found'
+        exit 1
+      fi
+    fi
+  fi
 fi
