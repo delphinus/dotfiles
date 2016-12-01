3253fc3904139af9e0372d111b75bab38eb23b22
delphinus <delphinus@remora.cx>
2016-12-01 17:18:21 +0900

    Fix set sock link

--- .zsh/set-ssh-auth-sock.zsh
+++ .zsh/set-ssh-auth-sock.zsh
@@ -1,20 +1,31 @@
+#!/usr/bin/env zsh
+sock_link=$HOME/.ssh/auth_sock
+
+function set_sock_link() {
+  sock=$(ls /tmp/**/Listeners || true)
+  if [ -n "$sock" ]; then
+    ln -fs $sock $sock_link
+  else
+    sock=$(ls /tmp/ssh*/agent* || true)
+    if [ -n "$sock" ]; then
+      ln -fs $sock $sock_link
+    else
+      >&2 echo 'sock file cannot be found'
+      exit 1
+    fi
+  fi
+}
+
 if [ -n "$SSH_AUTH_SOCK" ]; then
   if [ "$SSH_AUTH_SOCK" != "$HOME/.ssh/auth_sock" ]; then
     ln -fs $SSH_AUTH_SOCK $HOME/.ssh/auth_sock
-    export SSH_AUTH_SOCK=$HOME/.ssh/auth_sock
+    echo export SSH_AUTH_SOCK=$HOME/.ssh/auth_sock
   fi
   if ! [ -S "$SSH_AUTH_SOCK" ]; then
-    sock=$(ls /tmp/**/Listeners || true)
-    if [ -n "$sock" ]; then
-      ln -fs $sock $SSH_AUTH_SOCK
-    else
-      sock=$(ls /tmp/ssh*/agent* || true)
-      if [ -n "$sock" ]; then
-        ln -fs $sock $SSH_AUTH_SOCK
-      else
-        echo 'sock file cannot be found'
-        exit 1
-      fi
-    fi
+    set_sock_link
   fi
+else
+  rm -f $sock_link
+  set_sock_link
+  echo export SSH_AUTH_SOCK=$sock_link
 fi
