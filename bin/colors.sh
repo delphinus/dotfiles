a051f3976fa92d153ad6cfeae3b4181cd8a24c05
delphinus <delphinus@remora.cx>
2017-02-20 09:15:19 +0900

    Add 16colors mode for colors.sh

--- bin/colors.sh
+++ bin/colors.sh
@@ -49,10 +49,10 @@ cubes_6x6x6 () {  #originally g r b
     for g in 0 1 2 3 4 5; do
       for b in 0 1 2 3 4 5; do
         eval temp_r="\$$red"
-	eval temp_b="\$$blue"
-	eval temp_g="\$$green"
-	color=$((16 + (temp_r * 36) + (temp_g * 6) + temp_b))
-	printf "\033[48;5;${color}m  "
+        eval temp_b="\$$blue"
+        eval temp_g="\$$green"
+        color=$((16 + (temp_r * 36) + (temp_g * 6) + temp_b))
+        printf "\033[48;5;${color}m  "
       done
       printf "\033[m$delim"
     done
@@ -91,12 +91,12 @@ list_with_rgb_values () { #rbg
     for blue in 0 1 2 3 4 5; do
       for green in 0 1 2 3 4 5; do
         local color=$((16 + (red * 36) + (green * 6) + blue))
-	local rgb_red=$((red * 40))
-	local rgb_green=$((green * 40))
-	local rgb_blue=$((blue * 40))
-	rgb_red=$((rgb_red + rgb_red == 0 ? 0 : 55))
- 	rgb_green=$((rgb_green + rgb_green == 0 ? 0 : 55))
- 	rgb_blue=$((rgb_blue + rgb_blue == 0 ? 0 : 55))
+        local rgb_red=$((red * 40))
+        local rgb_green=$((green * 40))
+        local rgb_blue=$((blue * 40))
+        rgb_red=$((rgb_red + rgb_red == 0 ? 0 : 55))
+        rgb_green=$((rgb_green + rgb_green == 0 ? 0 : 55))
+        rgb_blue=$((rgb_blue + rgb_blue == 0 ? 0 : 55))
         printf "\033[38;5;${color}m %3u: %02x/%02x/%02x${delimiter1}" $color $rgb_red $rgb_green $rgb_blue
       done
       printf "$delimiter2"
@@ -108,24 +108,39 @@ system_color_table2 () {
 # from debian (versatile version)
   text="${1:-xXx}"
   size=${2:-1}
+  mode=${3:-8}
   delim=`rep_char $size ' '`
   side=
+  headers=(40 41 42 43 44 45 46 47)
+  fgs=(0 1 30 "1;30" 31 "1;31" 32 "1;32" 33 "1;33" 34 "1;34" 35 "1;35" 36 "1;36" 37 "1;37")
+  if [ $mode -eq 16 ]; then
+    headers+=(100 101 102 103 104 105 106 107)
+    fgs+=(90 "1;90" 91 "1;91" 92 "1;92" 93 "1;93" 94 "1;94" 95 "1;95" 96 "1;96" 97 "1;97")
+  fi
   if $title; then
     textlength=${#text}
     side=`rep_char $size ' '`
     head="$side       | ${delim}`rep_char $((textlength - 2)) ' '`0m  "
     local i=
-    for i in 40 41 42 43 44 45 46 47; do
-      head="$head$delim`rep_char $((textlength - 3)) ' '`  ${i}m  "
+    for i in ${headers[@]}; do
+      if [ $i -le 100 ]; then
+        space="  "
+      else
+        space=" "
+      fi
+      head="$head$delim`rep_char $((textlength - 3)) ' '`$space${i}m  "
     done
     echo
     echo "$head"
     echo "$head" | sed 's/[^|]/-/g;s/|/+/'
   fi
-  for fg in "" 1 30 "1;30" 31 "1;31" 32 "1;32" 33 "1;33" 34 "1;34" 35 "1;35" 36 "1;36" 37 "1;37"; do
+  for fg in ${fgs[@]}; do
+    if [ $fg = 0 ]; then
+      fg=""
+    fi
     if $title; then printf "$side%4sm  |$delim" "$fg"; fi
     printf "\033[${fg}m $text  "
-    for i in 40 41 42 43 44 45 46 47; do
+    for i in ${headers[@]}; do
       printf "$delim\033[${i}m  $text  \033[49m"
     done
     printf "\033[m\n"
@@ -140,8 +155,8 @@ system_color_table () {
   echo "        |   0m     40m     41m     42m     43m     44m     45m     46m     47m"
   echo " -------+-----------------------------------------------------------------------"
   for FGs in '    m' '   1m' '  30m' '1;30m' '  31m' '1;31m' '  32m' \
-	     '1;32m' '  33m' '1;33m' '  34m' '1;34m' '  35m' '1;35m' \
-	     '  36m' '1;36m' '  37m' '1;37m';
+             '1;32m' '  33m' '1;33m' '  34m' '1;34m' '  35m' '1;35m' \
+             '  36m' '1;36m' '  37m' '1;37m';
     do FG=${FGs// /}
     printf " $FGs  |\033[$FG  $TEXT  "
     for BG in 40m 41m 42m 43m 44m 45m 46m 47m;
@@ -161,12 +176,12 @@ delimiter2='\n'
 title=true
 
 #GetOptions (
-#	    'small'          => sub { $size = 0 },
-#	    'help'           => sub { die("help") },
-#	    'list'           => sub { list_with_rgb_values $delimiter1, $delimiter2 },
-#	    'long'           => sub { $delimiter1 = "\n"; $delimiter2 = "" },
-#	    'scales'         => sub {grayscale_24;colorscale_6 "r";colorscale_6 "g";colorscale_6 "b"; print "\n"; exit;},
-#	   );
+#          'small'          => sub { $size = 0 },
+#          'help'           => sub { die("help") },
+#          'list'           => sub { list_with_rgb_values $delimiter1, $delimiter2 },
+#          'long'           => sub { $delimiter1 = "\n"; $delimiter2 = "" },
+#          'scales'         => sub {grayscale_24;colorscale_6 "r";colorscale_6 "g";colorscale_6 "b"; print "\n"; exit;},
+#         );
 
 while getopts 0123456789ac:ghlpstwx FLAG; do
   case $FLAG in
@@ -190,8 +205,8 @@ if [ $colors -eq 8 ]; then
   else
     system_color_table2 "gYw" $((size - 1));
   fi
-#elif [ $colors -eq 16 ]; then :
-  #TODO
+elif [ $colors -eq 16 ]; then :
+  system_color_table2 "gYw" $((size - 1)) 16;
 elif [ $colors -eq 256 ]; then
   if [ $size -eq 0 ]; then
     cubes_6x6x6 g r b ""
