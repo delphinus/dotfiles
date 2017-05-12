49376b8b9a06c4229732bb5124529c96c5159ae4
delphinus <delphinus@remora.cx>
2017-05-12 22:04:21 +0900

    Set func for git-foresta if can execute

--- .zsh/git-foresta.zsh
+++ .zsh/git-foresta.zsh
@@ -1,11 +1,13 @@
-if type gf | grep alias > /dev/null; then
-  unalias gf
-fi
-if type gfa | grep alias > /dev/null; then
-  unalias gfa
-fi
+if (( $+commands[git-foresta] )); then
+  if type gf | grep alias > /dev/null; then
+    unalias gf
+  fi
+  if type gfa | grep alias > /dev/null; then
+    unalias gfa
+  fi
 
-function gf() { git-foresta --graph-symbol-merge='⍟' --style=1 $@ | less }
-function gfa() { git-foresta --all --graph-symbol-merge='⍟' --style=1 $@ | less }
-compdef _git gf=git-log
-compdef _git gfa=git-log
+  function gf() { git-foresta --graph-symbol-merge='⍟' --style=1 $@ | less }
+  function gfa() { git-foresta --all --graph-symbol-merge='⍟' --style=1 $@ | less }
+  compdef _git gf=git-log
+  compdef _git gfa=git-log
+fi
