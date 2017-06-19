af6f6a94d1a382aafe9e94905cdaeb4308b9f639
delphinus <delphinus@remora.cx>
2017-06-19 17:41:18 +0900

    Rewrite xargs in `pevo` for portability

--- .zsh/peco-view-sources.zsh
+++ .zsh/peco-view-sources.zsh
@@ -1,5 +1,5 @@
 function peco-view-sources() {
-    exec rg -n "$@" . | peco --exec 'awk -F : '"'"'{print "+" $2 " " $1}'"'"' 2>&1 | xargs -o view'
+    exec rg -n "$@" . | peco --exec 'awk -F : '"'"'{print "+" $2 " " $1}'"'"' 2>&1 | xargs sh -c '"'"'view "$@" < /dev/tty'"'"' view'
 }
 zle -N peco-view-sources
 bindkey '^x^v' peco-view-sources
