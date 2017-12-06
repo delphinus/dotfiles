01c7e027ec4cc1e0f66a928f42956f56d6ac3e94
delphinus <delphinus@remora.cx>
2017-12-06 23:51:58 +0900

    Add my_file_rec that is derived from file_rec

---
+++ vim/rplugin/python3/denite/source/my_file_rec.py
@@ -0,0 +1,18 @@
+# ============================================================================
+# FILE: my_file_rec.py
+# AUTHOR: delphinus <delphinus@remora.cx>
+# License: MIT license
+# ============================================================================
+
+from denite.source.file_rec import Source as Base
+
+
+class Source(Base):
+
+    def __init__(self, vim):
+        super().__init__(vim)
+        self.name = 'my_file_rec'
+
+    def highlight(self):
+        self.vim.command('syntax match {0}_Icon /[^/ \[\]]\+\s/ contained containedin={0}'.format(self.syntax_name))
+        self.vim.command('highlight default link {0}_Icon Function'.format(self.syntax_name))
