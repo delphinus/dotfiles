80b14ec304980af682651a023926f1fdd164d2ff
delphinus <delphinus@remora.cx>
2018-02-16 19:16:31 +0900

    Simplify setting for git-foresta

--- .zsh/git-foresta.zsh
+++ .zsh/git-foresta.zsh
@@ -6,8 +6,8 @@ if (( $+commands[git-foresta] )); then
     unalias gfa
   fi
 
-  function gf() { git-foresta --graph-symbol-merge='⍟' --style=1 $@ | less }
-  function gfa() { git-foresta --all --graph-symbol-merge='⍟' --style=1 $@ | less }
+  function gf() { git-foresta $@ | less }
+  function gfa() { git-foresta --all $@ | less }
   compdef _git gf=git-log
   compdef _git gfa=git-log
 fi
