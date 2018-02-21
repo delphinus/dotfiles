c412f4b57920f1dbc278c70d488e58535aa7b49f
delphinus <delphinus@remora.cx>
2018-02-21 15:03:15 +0900

    Add script to reverse ssh
    ref https://qiita.com/syoyo/items/d31e9db6851dfee3ef82

---
+++ bin/reverse-ssh-tunnel.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# http://qiita.com/syoyo/items/d31e9db6851dfee3ef82
+SSH_AUTH_SOCK=$(/bin/ls /tmp/**/Listeners)
+export SSH_AUTH_SOCK
+COMMAND='/usr/bin/ssh -N -f -R 20023:localhost:22 delphinus@remora.cx'
+/usr/bin/pgrep -f -x "$COMMAND" > /dev/null 2>&1 || $COMMAND
