b3b6789ee7993d10de0cc8ad17e405025b8f5815
delphinus <delphinus@remora.cx>
2018-03-27 18:19:14 +0900

    Add cron setting for godo

---
+++ bin/cron_godo.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+export GODO_MAIL_USER=delphinus@remora.cx
+export GODO_MAIL_RECIPIENTS=delphinus35@me.com
+export GODO_MAIL=delphinus@remora.cx
+pass="$(cat ~/.godo_gmail_password)"
+export GODO_MAIL_PASSWORD="$pass"
+
+now=$(date +'%F %T')
+export GODO_MAIL_SUBJECT="IPv4 アドレスを更新しました $now"
+
+"$HOME/.go/bin/godo" renew -z remora.cx -d remora.cx -m
