1f83ab1caf696212af63e1e0d9e51bd7a3746308
delphinus <delphinus@remora.cx>
2018-05-18 12:20:22 +0900

    Use autossh for reverse port forwarding

--- bin/reverse-ssh-tunnel.sh
+++ bin/reverse-ssh-tunnel.sh
@@ -2,5 +2,5 @@
 # http://qiita.com/syoyo/items/d31e9db6851dfee3ef82
 SSH_AUTH_SOCK=$(/bin/ls /tmp/**/Listeners)
 export SSH_AUTH_SOCK
-COMMAND='/usr/bin/ssh -N -f -R 20023:localhost:22 delphinus@remora.cx'
+COMMAND='/usr/local/bin/autossh -M 50000 -N -f -R 20023:localhost:22 delphinus@remora.cx'
 /usr/bin/pgrep -f -x "$COMMAND" > /dev/null 2>&1 || $COMMAND
