d154696849dac920556b609273c6d33977e0478e
delphinus <delphinus@remora.cx>
2018-06-30 10:16:44 +0900

    Fix for shellcheck

--- bin/tmux.sh
+++ bin/tmux.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
-if [ -n "$TMUX" ]; then
-  exec tmux $@
+#!/bin/bash
+if [[ -n $TMUX ]]; then
+  exec tmux "$@"
 fi
 
 tmux_cmd="tmux -u2 -f $HOME/git/dotfiles/.tmux.conf"
@@ -8,5 +8,5 @@ if tmux has-session 2> /dev/null; then
   "$HOME/git/dotfiles/bin/set_env_for_fssh.rb"
   $tmux_cmd attach "$@"
 else
-  $tmux_cmd $@
+  $tmux_cmd "$@"
 fi
