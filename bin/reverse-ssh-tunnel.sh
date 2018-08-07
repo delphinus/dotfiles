ad7ccb4c03ca889a0c33c7213fdf4c2a0ef80992
delphinus <delphinus@remora.cx>
2018-08-07 16:13:24 +0900

    Update reverse-ssh-tunnel.sh

--- bin/reverse-ssh-tunnel.sh
+++ bin/reverse-ssh-tunnel.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # http://qiita.com/syoyo/items/d31e9db6851dfee3ef82
-SSH_AUTH_SOCK=$(/bin/ls /tmp/**/Listeners)
+SSH_AUTH_SOCK=$("$HOME/.ghg/bin/gosshauth" list -f '{{.Path}}' | head -1)
 export SSH_AUTH_SOCK
 COMMAND='/usr/local/bin/autossh -M 50000 -N -f -R 20023:localhost:22 delphinus@remora.cx'
 /usr/bin/pgrep -f -x "$COMMAND" > /dev/null 2>&1 || $COMMAND
