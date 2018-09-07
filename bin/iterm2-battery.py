5113c4526ac647b74b5661e4d6b5d859530069aa
delphinus <delphinus@remora.cx>
2018-09-07 16:16:03 +0900

    Update iterm2-battery.py

--- bin/iterm2-battery.py
+++ bin/iterm2-battery.py
@@ -2,8 +2,23 @@
 
 import asyncio
 import iterm2
+from math import ceil, floor
+import re
 import sys
 from subprocess import CalledProcessError, check_output
+
+chars = [
+    "▏",
+    "▎",
+    "▍",
+    "▌",
+    "▋",
+    "▊",
+    "▉",
+    "█"]
+thunder = "ϟ"
+width = 3
+
 
 async def main(connection, argv):
     app = await iterm2.async_get_app(connection)
@@ -13,16 +28,34 @@ async def main(connection, argv):
         "Show remaining time for battery",
         [],
         "xx%",
-        60)
+        30)
 
     async def coro(knobs):
         try:
-            out = check_output(args=["/usr/bin/pmset", "-g", "batt"])
+            out = check_output(
+                args=["/usr/bin/pmset", "-g", "batt"]).decode('utf-8')
         except CalledProcessError as err:
             return "`pmset` cannot be executed"
-        if b"AC Power" in out:
+        try:
+            status = re.match(r'.*; (.*);', out, flags=re.S)[1]
+            percent = int(re.match(r'.*?(\d+)%', out, flags=re.S)[1])
+        except:
             return "🔌"
-        return "🔋"
+        if status == "charging":
+            mid = floor(width / 2)
+            battery = mid * " " + thunder + (width - mid) * " "
+        elif status == "discharging":
+            unit = len(chars)
+            total_char_len = len(chars) * width
+            char_len = floor(total_char_len * percent / 100)
+            full_len = floor(char_len / unit)
+            remained = char_len % unit
+            battery = chars[-1] * full_len
+            battery += '' if remained == 0 else chars[remained - 1]
+        matched = re.match(r'.*?(\d+:\d+)', out, flags=re.S)
+        elapsed = matched[1] if matched else ''
+        return "{0} {1} {2:d}% {3}".format(
+            "🔋", battery, percent, elapsed)
 
 
     await app.async_register_status_bar_component(component, coro)
