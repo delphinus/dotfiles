9316e98ca9a1e4436372afa80c5b710168db2e4a
delphinus <delphinus@remora.cx>
2018-09-11 17:02:50 +0900

    Fix up to use new logic to define components

--- bin/iterm2-battery.py
+++ bin/iterm2-battery.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 
 import asyncio
-import iterm2
+from iterm2 import StatusBarComponent, Registration, run
 from math import floor
 import re
-import sys
 from subprocess import CalledProcessError, check_output
 
 chars = [
@@ -21,9 +20,8 @@ width = 5
 interval = 30
 
 
-async def main(connection, argv):
-    app = await iterm2.async_get_app(connection)
-    component = iterm2.StatusBarComponent(
+async def main(connection):
+    component = StatusBarComponent(
         'Battery',
         'Show battery remaining',
         'Show remaining time for battery',
@@ -63,11 +61,11 @@ async def main(connection, argv):
         return '{0} |{1}| {2:d}% {3}'.format('🔋', battery, percent, elapsed)
 
 
-    await app.async_register_status_bar_component(component, coro)
+    await Registration.async_register_status_bar_component(
+        connection, component, coro)
 
     future = asyncio.Future()
     await connection.async_dispatch_until_future(future)
 
 
-if __name__ == '__main__':
-    iterm2.Connection().run(main, sys.argv)
+run(main)
