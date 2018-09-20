c5d3af5d8c8d0f002e1c394e8f97690da35b2397
delphinus <delphinus@remora.cx>
2018-09-20 09:07:37 +0900

    Do not use -f option to launch tmux
    See
    https://github.com/tmux-plugins/tpm/blob/master/docs/tpm_not_working.md

--- bin/tmux.sh
+++ bin/tmux.sh
@@ -3,7 +3,7 @@ if [[ -n $TMUX ]]; then
   exec tmux "$@"
 fi
 
-tmux_cmd="tmux -u2 -f $HOME/git/dotfiles/.tmux.conf"
+tmux_cmd="tmux -u2"
 if tmux has-session 2> /dev/null; then
   "$HOME/git/dotfiles/bin/set_env_for_fssh.rb"
   $tmux_cmd "$@" attach
