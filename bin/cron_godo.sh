f47acd6e188c6c126ebba038c1eca925e3b7d998
delphinus <delphinus@remora.cx>
2018-10-07 11:47:14 +0900

    Update IPv6 address with godo

--- bin/cron_godo.sh
+++ bin/cron_godo.sh
@@ -6,6 +6,7 @@ pass="$(cat ~/.godo_gmail_password)"
 export GODO_MAIL_PASSWORD="$pass"
 
 now=$(date +'%F %T')
-export GODO_MAIL_SUBJECT="IPv4 アドレスを更新しました $now"
+export GODO_MAIL_SUBJECT="アドレスを更新しました $now"
 
 "$HOME/.go/bin/godo" renew -z remora.cx -d remora.cx -m
+"$HOME/.go/bin/godo" renew -z remora.cx -d remora.cx -m -6
