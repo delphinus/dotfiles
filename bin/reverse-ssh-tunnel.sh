1d4244536fb75680d6bff60d15a426148ff039d8
delphinus <delphinus@remora.cx>
2019-01-08 10:58:59 +0900

    Use `git` to avoid 2FA auth

--- bin/reverse-ssh-tunnel.sh
+++ bin/reverse-ssh-tunnel.sh
@@ -2,5 +2,5 @@
 # http://qiita.com/syoyo/items/d31e9db6851dfee3ef82
 SSH_AUTH_SOCK=$("$HOME/.ghg/bin/gosshauth" list -f '{{.Path}}' | head -1)
 export SSH_AUTH_SOCK
-COMMAND='/usr/local/bin/autossh -M 50000 -N -f -R 20023:localhost:22 delphinus@remora.cx'
+COMMAND='/usr/local/bin/autossh -M 50000 -N -f -R 20023:localhost:22 git@remora.cx'
 /usr/bin/pgrep -f -x "$COMMAND" > /dev/null 2>&1 || $COMMAND
