1c5876b09df350d462574e3ac96c64f7f30ce163
delphinus <me@delphinus.dev>
2019-11-22 17:38:35 +0900

    Fix fzf-ghq to chdir

--- .config/fish/functions/fzf_z.fish
+++ .config/fish/functions/fzf_z.fish
@@ -1,5 +1,9 @@
 function fzf_z --description 'Move with FZF + z'
-  set select (z --list | perl -pe 's,$ENV{HOME},~,' | eval "fzf $FZF_DEFAULT_OPTS" | cut -c12-)
+  set select (z --list | \
+    perl -pe 's,$ENV{HOME},~,' | \
+    eval "fzf $FZF_DEFAULT_OPTS" | \
+    cut -c12- | \
+    perl -pe 's/^~(\w*)/(getpwnam($1 || $ENV{USER}))[7]/e')
   test -n "$select" ;and cd "$select"
   commandline -f repaint
 end
