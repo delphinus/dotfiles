9fc6f5a3ec2ca515172a2d94e2fe9a239731da2f
delphinus <me@delphinus.dev>
2019-11-29 20:05:12 +0900

    Update words checker

--- bin/git_prohibited_words_checker.rb
+++ bin/git_prohibited_words_checker.rb
@@ -1,4 +1,6 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
+
 require 'mail'
 require 'optparse'
 require 'pathname'
@@ -7,18 +9,25 @@ require 'syslog'
 
 DEFAULT_DIR           = Pathname('~/git/dotfiles').expand_path
 PROHIBITED_WORDS_FILE = Pathname('~/.git_prohibited_words').expand_path
-MAIL_PASSWORD_FILE    = Pathname('~/.git_prohibited_words_checker_password').expand_path
+MAIL_PASSWORD_FILE    =
+  Pathname('~/.git_prohibited_words_checker_password').expand_path
 
 class String
-  def red; STDOUT.isatty ? "\033[31m#{self}\033[0m" : self end
+  def red
+    STDOUT.isatty ? "\033[31m#{self}\033[0m" : self
+  end
 end
 
 class DirectoryNotFoundError < StandardError
-  def to_s; 'specified directory is not found' end
+  def to_s
+    'specified directory is not found'
+  end
 end
 
 class ProhibitedWordsNotFoundError < StandardError
-  def to_s; 'specified prohibited words file is not found' end
+  def to_s
+    'specified prohibited words file is not found'
+  end
 end
 
 params = ARGV.getopts 'd:p:v'
@@ -28,7 +37,7 @@ begin
               else
                 DEFAULT_DIR
               end
-  raise DirectoryNotFoundError unless Dir.exists? start_dir
+  raise DirectoryNotFoundError unless Dir.exist? start_dir
 
   prohibited_words_file = if params['p']
                             Pathname(params['p']).expand_path
@@ -36,14 +45,15 @@ begin
                             PROHIBITED_WORDS_FILE
                           end
   raise ProhibitedWordsNotFoundError unless File.readable? prohibited_words_file
-rescue => e
+rescue StandardError => e
   warn e.to_s.red
   exit 1
 end
 
-prohibited_words_pre = Regexp.new IO.readlines(prohibited_words_file).map(&:chomp).join '|'
+prohibited_words_pre =
+  Regexp.new IO.readlines(prohibited_words_file).map(&:chomp).join '|'
 
-ignore_names_re = %r[
+ignore_names_re = %r{
   \.exe$ |
   \.mypy_cache\b |
   \.pyc$ |
@@ -59,15 +69,17 @@ ignore_names_re = %r[
   \A\.zsh/zsh-notify/ |
   \Abin/local_perl\.sh\z |
   \Asubmodules/ |
-  \bnode_modules\b |
-]x
+  \bnode_modules\b
+}x
 
 scan_count = 0
 skip_count = 0
 
-errors = Dir.glob(start_dir + '**/*', File::FNM_DOTMATCH).map do |path|
+files = Dir.glob(start_dir + '**/*', File::FNM_DOTMATCH).map do |path|
   Pathname(path).relative_path_from start_dir
-end.select do |path|
+end
+
+scanned = files.select do |path|
   if File.file?(path) && path.to_s !~ ignore_names_re
     scan_count += 1
     true
@@ -75,50 +87,56 @@ end.select do |path|
     skip_count += 1
     false
   end
-end.each_with_object [] do |file, ary|
+end
+
+errors = scanned.each_with_object [] do |file, ary|
   ary << file if IO.read(file).scrub =~ prohibited_words_pre
 end
 
-mail_body_header = sprintf 'dotfiles scan for prohibited words; scaned: %d file(s), skipped: %d file(s), found: %d file(s)', scan_count, skip_count, errors.size
+mail_body_header =
+  format 'dotfiles scan for prohibited words; scanned: %<scanned>d file(s), skipped: %<skipped>d file(s), found: %<errors>d file(s)',
+         scanned: scan_count,
+         skipped: skip_count,
+         errors: errors.size
 
 puts(mail_body_header) if params['v']
 
-Syslog.open File.basename($0) do |syslog|
+Syslog.open File.basename($PROGRAM_NAME) do |syslog|
   syslog.notice mail_body_header
 end
 
-exit if errors.count == 0
+exit if errors.count.zero?
 
-Syslog.open File.basename($0) do |syslog|
+Syslog.open File.basename($PROGRAM_NAME) do |syslog|
   syslog.warning "prohibited words found in #{errors.join(', ')}"
 end
 
 unless File.readable? MAIL_PASSWORD_FILE
-  Syslog.open File.basename($0) do |syslog|
+  Syslog.open File.basename($PROGRAM_NAME) do |syslog|
     syslog.warning "#{MAIL_PASSWORD_FILE} is not readble"
   end
   exit 1
 end
 
 mail_body = mail_body_header + "\n\n"
-errors.each do |e|
-  mail_body += "  - #{e}"
+errors.each do |error|
+  mail_body += "  - #{error}"
 end
 
 mail = Mail.new do
-  from    'root@remora.cx'
-  to      'delphinus@remora.cx'
-  subject '[prohibited words checker] %s %s report' % [Socket.gethostname, Date.today.strftime('%Y/%m/%d')]
+  from    'me@delphinus.dev'
+  to      'me@delphinus.dev'
+  subject format('[prohibited words checker] %<host>s %<ymd>s report',
+                 host: Socket.gethostname, ymd: Date.today.strftime('%Y/%m/%d'))
   body    mail_body
 end
 
-mail.delivery_method :smtp, {
-  address: 'smtp.gmail.com',
-  port: 587,
-  domain: 'smtp.gmail.com',
-  user_name: 'me@delphinus.dev',
-  password: IO.read(MAIL_PASSWORD_FILE).chomp,
-  authentication: :plain,
-}
+mail.delivery_method :smtp,
+                     address: 'smtp.gmail.com',
+                     port: 587,
+                     domain: 'smtp.gmail.com',
+                     user_name: 'me@delphinus.dev',
+                     password: IO.read(MAIL_PASSWORD_FILE).chomp,
+                     authentication: :plain
 
 mail.deliver
