25b4485c9be6a69e00b15a3f3ea74604b7b4e8d0
delphinus <me@delphinus.dev>
2020-01-21 12:20:51 +0900

    Add paths for Perl scripts

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -18,7 +18,8 @@ assert_user_paths \
   ~/git/dotfiles/bin \
   ~/Library/Python/3.7/bin \
   ~/.gem/ruby/2.3.0/bin \
-  /usr/local/opt/llvm/bin
+  /usr/local/opt/llvm/bin \
+  /usr/local/opt/perl/bin
 
 set nord0 2e3440
 set nord1 3b4252
