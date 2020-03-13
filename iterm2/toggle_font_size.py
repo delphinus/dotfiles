3fbb08ff61006587e389fe158f803df40f67d46f
delphinus <me@delphinus.dev>
2020-03-13 10:12:46 +0900

    Update font size changer

--- iterm2/toggle_font_size.py
+++ iterm2/toggle_font_size.py
@@ -9,16 +9,16 @@ from iterm2 import (
     async_get_app,
     run_forever,
 )
-from typing import Union
 from re import compile
+from typing import Optional
 
-SMALL = 14
-MEDIUM = 16
-
+SMALL = {'normal':11, 'non-ascii':14}
+MEDIUM = {'normal':13, 'non-ascii':16}
+LARGE = {'normal':16, 'non-ascii':20}
+NAME_SIZE_RE = compile(r"^(.* )(\d*)$")
 
 async def main(connection: Connection) -> None:
     app: App = await async_get_app(connection)
-    name_and_size = compile(r"^(.* )(\d*)$")
 
     @RPC
     async def toggle_font_size(session_id: str) -> None:
@@ -26,19 +26,25 @@ async def main(connection: Connection) -> None:
         if not session:
             return
         profile: Profile = await session.async_get_profile()
-        match = name_and_size.search(profile.normal_font)
-        if not match:
-            return
-        name = match.group(1)
-        size = int(match.group(2))
-        toggled_size = SMALL if size == MEDIUM else MEDIUM
-        replacement = name + str(toggled_size)
         new_profile = LocalWriteOnlyProfile()
-        new_profile.set_normal_font(replacement)
+        normal_font = font_name(profile.normal_font, 'normal')
+        non_ascii_font = font_name(profile.non_ascii_font, 'non-ascii')
+        if normal_font and non_ascii_font:
+            new_profile.set_normal_font(normal_font)
+            new_profile.set_non_ascii_font(non_ascii_font)
 
         await session.async_set_profile_properties(new_profile)
 
     await toggle_font_size.async_register(connection)
+
+def font_name(current:str, kind: str) -> Optional[str]:
+    if m := NAME_SIZE_RE.search(current):
+        name = m.group(1)
+        current = int(m.group(2))
+        size = (
+            LARGE if current == MEDIUM[kind] else SMALL if current == LARGE[kind] else MEDIUM
+        )
+        return name + str(size[kind])
 
 
 if __name__ == "__main__":
