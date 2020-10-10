6fe13354fb821ba46a379d818ac742fc4365b3b8
delphinus <me@delphinus.dev>
2020-10-10 23:02:53 +0900

    Add EX_LARGE fonts for iterm2

--- iterm2/toggle_font_size.py
+++ iterm2/toggle_font_size.py
@@ -12,10 +12,12 @@ from iterm2 import (
 from re import compile
 from typing import Optional
 
-SMALL = {'normal':11, 'non-ascii':14}
-MEDIUM = {'normal':13, 'non-ascii':16}
-LARGE = {'normal':16, 'non-ascii':20}
+SMALL = {"normal": 11, "non-ascii": 14}
+MEDIUM = {"normal": 13, "non-ascii": 16}
+LARGE = {"normal": 16, "non-ascii": 20}
+EX_LARGE = {"normal": 20, "non-ascii": 24}
 NAME_SIZE_RE = compile(r"^(.* )(\d*)$")
+
 
 async def main(connection: Connection) -> None:
     app: App = await async_get_app(connection)
@@ -27,8 +29,8 @@ async def main(connection: Connection) -> None:
             return
         profile: Profile = await session.async_get_profile()
         new_profile = LocalWriteOnlyProfile()
-        normal_font = font_name(profile.normal_font, 'normal')
-        non_ascii_font = font_name(profile.non_ascii_font, 'non-ascii')
+        normal_font = font_name(profile.normal_font, "normal")
+        non_ascii_font = font_name(profile.non_ascii_font, "non-ascii")
         if normal_font and non_ascii_font:
             new_profile.set_normal_font(normal_font)
             new_profile.set_non_ascii_font(non_ascii_font)
@@ -37,12 +39,19 @@ async def main(connection: Connection) -> None:
 
     await toggle_font_size.async_register(connection)
 
-def font_name(current:str, kind: str) -> Optional[str]:
+
+def font_name(current: str, kind: str) -> Optional[str]:
     if m := NAME_SIZE_RE.search(current):
         name = m.group(1)
-        current = int(m.group(2))
+        current_size = int(m.group(2))
         size = (
-            LARGE if current == MEDIUM[kind] else SMALL if current == LARGE[kind] else MEDIUM
+            EX_LARGE
+            if current_size == LARGE[kind]
+            else LARGE
+            if current_size == MEDIUM[kind]
+            else MEDIUM
+            if current_size == SMALL[kind]
+            else SMALL
         )
         return name + str(size[kind])
 
