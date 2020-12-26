eb9cb341e809bc244b07dac966f8c9e227ec427e
delphinus <me@delphinus.dev>
2020-12-26 20:11:23 +0900

    Do not short on fzf
    Because fzf sorts non-numerically. The results of z is already sorted,
    so use it as it is.

--- .config/fish/functions/fzf_z.fish
+++ .config/fish/functions/fzf_z.fish
@@ -4,7 +4,7 @@ function fzf_z --description 'Move with FZF + z'
 
   z --list | \
     perl -pe 's,$ENV{HOME},~,' | \
-    eval "fzf $FZF_DEFAULT_OPTS" | \
+    eval "fzf +s $FZF_DEFAULT_OPTS" | \
     cut -c12- | \
     perl -pe 's/^~(\w*)/(getpwnam($1 || $ENV{USER}))[7]/e' | \
     read -l select
