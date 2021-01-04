352930f183a1492f7f4276984b91e7c39fab645a
delphinus <me@delphinus.dev>
2021-01-04 21:27:10 +0900

    Use treesitter instead of yats.vim

--- .vim/lua/packers/start.lua
+++ .vim/lua/packers/start.lua
@@ -220,7 +220,6 @@ return {
 
   -- Syntax {{{
   {'Glench/Vim-Jinja2-Syntax'},
-  {'HerringtonDarkholme/yats.vim'},
   {'Vimjas/vim-python-pep8-indent'},
   {'aklt/plantuml-syntax'},
   {'cespare/vim-toml'},
