c6afb8426df708925c22b584cc8ba5433d46464d
delphinus <me@delphinus.dev>
2021-01-23 15:56:35 +0900

    Add mappings for packer

--- .vim/lua/packers/init.lua
+++ .vim/lua/packers/init.lua
@@ -14,3 +14,12 @@ vim.cmd[[command! PackerUpdate  lua run_packer'update']]
 vim.cmd[[command! PackerSync    lua run_packer'sync']]
 vim.cmd[[command! PackerClean   lua run_packer'clean']]
 vim.cmd[[command! PackerCompile lua run_packer'compile']]
+
+if pcall(vim.cmd, [[packadd vimpeccable]]) then
+  local vimp = require'vimp'
+  vimp.nnoremap('<Leader>pi', function() run_packer'install' end)
+  vimp.nnoremap('<Leader>pu', function() run_packer'update' end)
+  vimp.nnoremap('<Leader>ps', function() run_packer'sync' end)
+  vimp.nnoremap('<Leader>pc', function() run_packer'clean' end)
+  vimp.nnoremap('<Leader>po', function() run_packer'compile' end)
+end
