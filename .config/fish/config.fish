275aadd130445a73b6fb9fea0ca9bc2e6157b175
delphinus <me@delphinus.dev>
2021-02-08 10:50:43 +0900

    Arrange colors for fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -21,47 +21,54 @@ if test "$paths" != "$fish_user_paths"
   set -U fish_user_paths $paths
 end
 
-set -l nord0 2e3440
-set -l nord1 3b4252
-set -l nord2 434c5e
-set -l nord3 4c566a
-set -l nord4 d8dee9
-set -l nord5 e5e9f0
-set -l nord6 eceff4
-set -l nord7 8fbcbb
-set -l nord8 88c0d0
-set -l nord9 81a1c1
-set -l nord10 5e81ac
-set -l nord11 bf616a
-set -l nord12 d08770
-set -l nord13 ebcb8b
-set -l nord14 a3be8c
-set -l nord15 b48ead
+set -l nord0 2e3440 # #2e3440
+set -l nord1 3b4252 # #3b4252
+set -l nord2 434c5e # #434c5e
+set -l nord3 4c566a # #4c566a
+set -l nord4 d8dee9 # #d8dee9
+set -l nord5 e5e9f0 # #e5e9f0
+set -l nord6 eceff4 # #eceff4
+set -l nord7 8fbcbb # #8fbcbb
+set -l nord8 88c0d0 # #88c0d0
+set -l nord9 81a1c1 # #81a1c1
+set -l nord10 5e81ac # #5e81ac
+set -l nord11 bf616a # #bf616a
+set -l nord12 d08770 # #d08770
+set -l nord13 ebcb8b # #ebcb8b
+set -l nord14 a3be8c # #a3be8c
+set -l nord15 b48ead # #b48ead
 
-set fish_color_normal $nord4
-set fish_color_command $nord9
-set fish_color_quote $nord14
-set fish_color_redirection $nord9
-set fish_color_end $nord6
-#set fish_color_error $nord11
-set fish_color_error $nord12
-set fish_color_param $nord4
-set fish_color_comment $nord3
-set fish_color_match $nord8
-set fish_color_search_match $nord8
-set fish_color_operator $nord9
-set fish_color_escape $nord13
-set fish_color_cwd $nord8
-#set fish_color_autosuggestion $nord6
-set fish_color_autosuggestion $nord3
-set fish_color_user $nord4
-set fish_color_host $nord9
-set fih_color_cancel $nord15
-set fish_pager_color_prefix $nord13
-set fish_pager_color_completion $nord6
-set fish_pager_color_description $nord10
-set fish_pager_color_progress $nord12
-set fish_pager_color_secondary $nord1s
+set fish_color_normal $nord4 # #d8dee9
+set fish_color_command $nord9 # #81a1c1
+set fish_color_quote $nord14 # #a3be8c
+set fish_color_redirection $nord9 # #81a1c1
+set fish_color_end $nord6 # #eceff4
+#set fish_color_error $nord11 # #bf616a
+set fish_color_error $nord12 # #d08770
+set fish_color_param $nord4 # #d8dee9
+set fish_color_comment $nord3 # #4c566a
+set fish_color_match $nord8 # #88c0d0
+set fish_color_search_match $nord8 # #88c0d0
+set fish_color_operator $nord9 # #81a1c1
+set fish_color_escape $nord13 # #ebcb8b
+set fish_color_cwd $nord8 # #88c0d0
+#set fish_color_autosuggestion $nord6 # #eceff4
+set fish_color_autosuggestion $nord3 # #4c566a
+set fish_color_user $nord4 # #d8dee9
+set fish_color_host $nord9 # #81a1c1
+set fih_color_cancel $nord15 # #b48ead
+set fish_pager_color_prefix $nord13 # #ebcb8b
+set fish_pager_color_completion $nord6 # #eceff4
+set fish_pager_color_description $nord10 # #5e81ac
+set fish_pager_color_progress $nord12 # #d08770
+set fish_pager_color_secondary $nord1 # #3b4252
+
+set hydro_color_duration $nord13 # #ebcb8b
+set hydro_color_error $nord12 # #d08770
+set hydro_color_git $nord14 # #a3be8c
+set hydro_color_pwd $nord10 # #5e81ac
+set hydro_fetch true
+set hydro_symbol_prompt \e'[1;31m'❱\e'[1;33m'❱\e'[1;32m'❱\e'[m'
 
 alias cp 'cp -i'
 alias ln 'ln -i'
