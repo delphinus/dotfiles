f1798ae9d8cebffb8100aa8aca002b092bcbec1f
delphinus <me@delphinus.dev>
2021-02-13 20:07:20 +0900

    Use more robust way to detect dir

--- .vim/lua/packers/init.lua
+++ .vim/lua/packers/init.lua
@@ -1,7 +1,7 @@
 local install_path = vim.fn.stdpath'data'..'/site/pack/packer/opt/packer.nvim'
-if vim.fn.isdirectory(install_path) == 0 then
+local st = vim.loop.fs_stat(install_path)
+if not st or st.type ~= 'directory' then
   os.execute('git clone https://github.com/wbthomason/packer.nvim '..install_path)
-  vim.cmd'packadd packer.nvim'
 end
 
 function _G.run_packer(method)
