317c5cbb246fa2698105ad3b0731f45996a11122
delphinus <me@delphinus.dev>
2021-04-13 08:55:51 +0900

    Fix up settings for fish_git_prompt

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -75,17 +75,23 @@ set __fish_git_prompt_showdirtystate true
 set __fish_git_prompt_showstashstate true
 set __fish_git_prompt_showuntrackedfiles true
 set __fish_git_prompt_showupstream auto
-set __fish_git_prompt_show_informative_status true
-set __fish_git_prompt_char_upstream_prefix ''
-set __fish_git_prompt_char_upstream_ahead  ↑
+set __fish_git_prompt_showcolorhints true
+
+# This is extremely slow
+#set __fish_git_prompt_show_informative_status true
+
+set __fish_git_prompt_char_cleanstate ✓
+set __fish_git_prompt_char_dirtystate ∑
+set __fish_git_prompt_char_invalidstate ✗
+set __fish_git_prompt_char_stagedstate ●
+set __fish_git_prompt_char_stashstate ∏
+set __fish_git_prompt_char_stateseparator ❘
+set __fish_git_prompt_char_untrackedfiles …
+set __fish_git_prompt_char_upstream_ahead ↑
 set __fish_git_prompt_char_upstream_behind ↓
-set __fish_git_prompt_char_stateseparator  |
-set __fish_git_prompt_char_dirtystate      ∑
-set __fish_git_prompt_char_invalidstate    ✗
-set __fish_git_prompt_char_stagedstate     ●
-set __fish_git_prompt_char_untrackedfiles  …
-set __fish_git_prompt_char_stashstate      ∏
-set __fish_git_prompt_char_cleanstate      ✓
+set __fish_git_prompt_char_upstream_diverged ≠
+set __fish_git_prompt_char_upstream_equal =
+set __fish_git_prompt_char_upstream_prefix ''
 
 alias cp 'cp -i'
 alias ln 'ln -i'
