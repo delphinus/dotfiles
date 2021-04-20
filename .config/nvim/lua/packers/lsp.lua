b5054dfcff6dc4fd866ef155afa9bb045c5cbd5d
delphinus <me@delphinus.dev>
2021-04-21 07:45:48 +0900

    Use nvim-compe

--- .config/nvim/lua/packers/lsp.lua
+++ .config/nvim/lua/packers/lsp.lua
@@ -258,6 +258,43 @@ return {
   }, -- }}}
   ]=]
 
+  {
+    'hrsh7th/nvim-compe',
+    requires = {
+      {'andersevenrud/compe-tmux'},
+    },
+    config = function()
+      require'compe'.setup {
+        enabled = true,
+        autocomplete = true,
+        debug = false,
+        min_length = 1,
+        preselect = 'enable',
+        throttle_time = 80,
+        source_timeout = 200,
+        incomplete_delay = 400,
+        max_abbr_width = 100,
+        max_kind_width = 100,
+        max_menu_width = 100,
+        documentation = true,
+
+        source = {
+          buffer = true,
+          calc = true,
+          nvim_lsp = true,
+          nvim_lua = true,
+          nvim_treesitter = true,
+          path = true,
+          spell = true,
+          tags = true,
+          vsnip = true,
+
+          tmux = true,
+        },
+      }
+    end,
+  },
+
   { -- {{{ nvim-treesitter
     'nvim-treesitter/nvim-treesitter',
     requires = {
