bc59465642a27eb22d5e483bacf7ed8b8196d262
delphinus <me@delphinus.dev>
2021-05-07 16:35:30 +0900

    Remove trailing spaces

--- .config/nvim/lua/packers/start.lua
+++ .config/nvim/lua/packers/start.lua
@@ -440,12 +440,12 @@ return {
     'edluffy/specs.nvim',
     config = function()
       local specs = require'specs'
-      specs.setup{ 
+      specs.setup{
         show_jumps  = true,
         min_jump = 30,
         popup = {
           delay_ms = 0, -- delay before popup displays
-          inc_ms = 10, -- time increments used for fade/resize effects 
+          inc_ms = 10, -- time increments used for fade/resize effects
           blend = 10, -- starting blend, between 0-100 (fully transparent), see :h winblend
           width = 40,
           winhl = 'PMenu',
