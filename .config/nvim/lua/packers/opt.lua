20835abc9a9e50f4a990c1ddbd5a3a9d3e1144a7
delphinus <me@delphinus.dev>
2021-07-05 10:59:32 +0900

    Move compe to opt

--- .config/nvim/lua/packers/opt.lua
+++ .config/nvim/lua/packers/opt.lua
@@ -391,6 +391,47 @@ return {
   -- {'delphinus/vim-emacscommandline', event = {'CmdlineEnter'}},
   {'delphinus/vim-emacscommandline'},
 
+  {
+    'andersevenrud/compe-tmux',
+    event = {'InsertEnter'},
+  },
+
+  {
+    'hrsh7th/nvim-compe',
+    event = {'InsertEnter'},
+    after = {'compe-tmux'},
+    config = function()
+      require'compe'.setup {
+        enabled = true,
+        autocomplete = true,
+        debug = false,
+        min_length = 1,
+        preselect = 'enable',
+        throttle_time = 80,
+        source_timeout = 200,
+        incomplete_delay = 400,
+        max_abbr_width = 100,
+        max_kind_width = 100,
+        max_menu_width = 100,
+        documentation = true,
+
+        source = {
+          buffer = true,
+          calc = true,
+          nvim_lsp = true,
+          nvim_lua = true,
+          nvim_treesitter = true,
+          path = true,
+          spell = true,
+          tags = true,
+          vsnip = true,
+
+          tmux = true,
+        },
+      }
+    end,
+  },
+
   {'itchyny/vim-cursorword', event = {'FocusLost', 'CursorHold'}},
 
   {
