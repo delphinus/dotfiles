96004235760ef0c2aed8f26a512a7486f1da9f9f
delphinus <me@delphinus.dev>
2021-07-07 12:37:45 +0900

    Show messages for PackerCompile

--- .config/nvim/lua/packers/init.lua
+++ .config/nvim/lua/packers/init.lua
@@ -16,11 +16,11 @@ vim.cmd[[command! PackerClean   lua run_packer'clean']]
 vim.cmd[[command! PackerCompile lua run_packer'compile']]
 vim.cmd[[command! PackerProfile lua run_packer'profile_output']]
 
-local ok, m = pcall(require, 'mappy')
-if ok then
-  m.nnoremap('<Leader>pi', function() run_packer'install' end)
-  m.nnoremap('<Leader>pu', function() run_packer'update' end)
-  m.nnoremap('<Leader>ps', function() run_packer'sync' end)
-  m.nnoremap('<Leader>pc', function() run_packer'clean' end)
-  m.nnoremap('<Leader>po', function() run_packer'compile' end)
-end
+local m = require'mappy'
+m.nnoremap('<Leader>ps', function() _G.run_packer'sync' end)
+m.nnoremap('<Leader>po', function()
+  _G.run_packer'compile'
+  vim.api.nvim_echo({
+    {'[packer] Compile complete!', 'WarningMsg'},
+  }, true, {})
+end)
