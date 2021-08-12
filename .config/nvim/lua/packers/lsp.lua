bb9b8939cdc7ab3c3238367dbb45befbdd8b2b61
delphinus <me@delphinus.dev>
2021-08-12 13:28:38 +0900

    Add tree siter query linter

--- .config/nvim/lua/packers/lsp.lua
+++ .config/nvim/lua/packers/lsp.lua
@@ -363,6 +363,11 @@ return {
           updatetime = 25,
           persist_queries = false,
         },
+        query_linter = {
+          enable = true,
+          use_virtual_text = true,
+          lint_events = {"BufWrite", "CursorHold"},
+        },
         -- TODO: disable because too slow in C
         --[[
         refactor = {
