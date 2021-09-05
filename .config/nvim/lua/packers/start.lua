37d9cd1dd81bd7cdf1feb3b4c630688f7e941f1d
delphinus <me@delphinus.dev>
2021-09-05 10:17:28 +0900

    Ignore to read PWD from bashls

--- .config/nvim/lua/packers/start.lua
+++ .config/nvim/lua/packers/start.lua
@@ -7,7 +7,7 @@ return {
     'ahmedkhalf/project.nvim',
     config = function()
       require'project_nvim'.setup{
-        ignore_lsp = {'efm', 'tsserver'},
+        ignore_lsp = {'bashls', 'efm', 'tsserver'},
         patterns = {'.git'},
         silent_chdir = false,
       }
