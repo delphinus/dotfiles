366377079778c6cb5b5496314e147bcaf6f5a0c8
delphinus <me@delphinus.dev>
2021-10-15 12:54:04 +0900

    Remove self-created func and use :Notifications

--- .config/nvim/lua/packers/start.lua
+++ .config/nvim/lua/packers/start.lua
@@ -240,26 +240,6 @@ return {
         stages = stages,
       }
       vim.notify = notify
-
-      notify.print_history = function()
-        local color = {
-          DEBUG = 'NotifyDEBUGTitle',
-          TRACE = 'NotifyTRACETitle',
-          INFO = 'NotifyINFOTitle',
-          WARN = 'NotifyWARNTitle',
-          ERROR = 'NotifyERRORTitle',
-        }
-        for _, m in ipairs(notify.history()) do
-          vim.api.nvim_echo({
-            {vim.fn.strftime('%FT%T', m.time), 'Identifier'},
-            {' ', 'Normal'},
-            {m.level, color[m.level] or 'Title'},
-            {' ', 'Normal'},
-            {table.concat(m.message, ' '), 'Normal'},
-          }, false, {})
-        end
-      end
-      vim.cmd([[command! Message :lua require'notify'.print_history()<CR>]])
     end,
   },
 
