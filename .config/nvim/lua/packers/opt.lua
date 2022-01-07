3131dffac71e1e7b2dce36651f65fee396186015
delphinus <me@delphinus.dev>
2022-01-07 19:24:43 +0900

    Use vim.keymap instead of dwm:map()

--- .config/nvim/lua/packers/opt.lua
+++ .config/nvim/lua/packers/opt.lua
@@ -321,17 +321,17 @@ return {
         master_pane_count = 1,
         master_pane_width = "60%",
       }
-      dwm.map("<C-j>", "<C-w>w")
-      dwm.map("<C-k>", "<C-w>W")
-      dwm.map("<A-CR>", dwm.focus)
-      dwm.map("<C-@>", dwm.focus)
-      dwm.map("<C-Space>", dwm.focus)
-      dwm.map("<C-l>", dwm.grow)
-      dwm.map("<C-h>", dwm.shrink)
-      dwm.map("<C-n>", dwm.new)
-      dwm.map("<C-q>", dwm.rotateLeft)
-      dwm.map("<C-s>", dwm.rotate)
-      dwm.map("<C-c>", function()
+      vim.keymap.set("n", "<C-j>", "<C-w>w")
+      vim.keymap.set("n", "<C-k>", "<C-w>W")
+      vim.keymap.set("n", "<A-CR>", dwm.focus)
+      vim.keymap.set("n", "<C-@>", dwm.focus)
+      vim.keymap.set("n", "<C-Space>", dwm.focus)
+      vim.keymap.set("n", "<C-l>", dwm.grow)
+      vim.keymap.set("n", "<C-h>", dwm.shrink)
+      vim.keymap.set("n", "<C-n>", dwm.new)
+      vim.keymap.set("n", "<C-q>", dwm.rotateLeft)
+      vim.keymap.set("n", "<C-s>", dwm.rotate)
+      vim.keymap.set("n", "<C-c>", function()
         -- TODO: copied logic from require'scrollbar'.clear
         local state = vim.b.scrollbar_state
         if state and state.winnr then
