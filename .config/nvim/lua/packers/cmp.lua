1e0b36862cc3aae66bbcc6345958827da47ba66b
delphinus <me@delphinus.dev>
2022-02-04 22:52:30 +0900

    Disable <CR> not to stuck

--- .config/nvim/lua/packers/cmp.lua
+++ .config/nvim/lua/packers/cmp.lua
@@ -35,7 +35,7 @@ return {
           ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
           ["<C-y>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
           ["<C-e>"] = cmp.mapping { i = cmp.mapping.abort(), c = cmp.mapping.close() },
-          ["<CR>"] = cmp.mapping.confirm { select = true },
+          --["<CR>"] = cmp.mapping.confirm { select = true },
         },
         sources = cmp.config.sources({
           { name = "nvim_lsp" },
