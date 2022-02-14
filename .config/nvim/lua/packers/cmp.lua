e85404a02e3d09cbf09926d6f9c797b0306c3829
delphinus <me@delphinus.dev>
2022-02-14 13:56:48 +0900

    Add documentation border for nvim-cmp

--- .config/nvim/lua/packers/cmp.lua
+++ .config/nvim/lua/packers/cmp.lua
@@ -272,6 +272,9 @@ return {
             },
           },
         },
+        documentation = {
+          border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+        },
       }
       cmp.setup.cmdline("/", { sources = { { name = "buffer" } } })
       cmp.setup.cmdline(":", { sources = cmp.config.sources({ { name = "path" } }, { { name = "cmdline" } }) })
