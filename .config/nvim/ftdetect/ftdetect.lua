5bbd15d3ab50281b16ef0f1f9d37971cd0663c6e
delphinus <me@delphinus.dev>
2022-03-03 15:00:56 +0900

    Use the native API to set autocmds

--- .config/nvim/ftdetect/ftdetect.lua
+++ .config/nvim/ftdetect/ftdetect.lua
@@ -1,54 +1,52 @@
-require("agrp").set {
-  my_ftdetect = {
-    ["BufNewFile,BufRead"] = {
-      { "*.gs,.amethyst", [[set filetype=javascript]] },
-      { "*.jbuilder ", [[set filetype=ruby]] },
-      { "*pentadactylrc*,*.penta ", [[set filetype=pentadactyl]] },
-      {
-        "*.xt",
-        function()
-          fn["dist#ft#FTperl"]()
-        end,
-      },
-      -- TODO: filetype.lua does not detect script with shebang.
-      {
-        "*",
-        function()
-          local top = api.buf_get_lines(0, 0, 1, false)[1]
-          if top then
-            if top:match "^#!" then
-              local bin = top:match "^.+/([^/ ]+)"
-              if bin == "env" then
-                bin = top:match "env +([^ ]+)"
-              end
-              if bin then
-                vim.cmd("setfiletype " .. bin)
-              end
-            end
-          end
-        end,
-      },
-      {
-        "*.conf",
-        function()
-          if vim.opt.filetype:get() == "tmux" then
-            return
-          end
-          local sep = package.config:sub(1, 1)
-          for _, item in ipairs(vim.split(fn.expand "%:p", sep)) do
-            if item:match "tmux" then
-              vim.cmd [[setfiletype tmux]]
-              return
-            end
-          end
-        end,
-      },
-      { "*.tt2 ", [[setf tt2html]] },
-      { "*.tt ", [[setf tt2html]] },
-      { ".zpreztorc ", [[setf zsh]] },
-      { "*.plist,*.ttx ", [[setf xml]] },
-      { "*.applescript", [[setf applescript]] },
-      { "*.pm", [[setf perl]] },
-    },
-  },
-}
+local function ftdetect(pattern)
+  return function(cb)
+    if type(cb) == "string" then
+      api.create_autocmd({ "BufNewFile", "BufRead" }, { pattern = pattern, command = cb })
+    else
+      api.create_autocmd({ "BufNewFile", "BufRead" }, { pattern = pattern, callback = cb })
+    end
+  end
+end
+
+ftdetect "*.gs,.amethyst" [[set filetype=javascript]]
+ftdetect "*.jbuilder" [[set filetype=ruby]]
+ftdetect "*pentadactylrc*,*.penta" [[set filetype=pentadactyl]]
+ftdetect "*.tt2" [[setf tt2html]]
+ftdetect "*.tt" [[setf tt2html]]
+ftdetect ".zpreztorc" [[setf zsh]]
+ftdetect "*.plist,*.ttx" [[setf xml]]
+ftdetect "*.applescript" [[setf applescript]]
+ftdetect "*.pm" [[setf perl]]
+
+ftdetect "*.xt"(function()
+  fn["dist#ft#FTperl"]()
+end)
+
+-- TODO: filetype.lua does not detect script with shebang.
+ftdetect "*"(function()
+  local top = api.buf_get_lines(0, 0, 1, false)[1]
+  if top then
+    if top:match "^#!" then
+      local bin = top:match "^.+/([^/ ]+)"
+      if bin == "env" then
+        bin = top:match "env +([^ ]+)"
+      end
+      if bin then
+        vim.cmd("setfiletype " .. bin)
+      end
+    end
+  end
+end)
+
+ftdetect "*.conf"(function()
+  if vim.opt.filetype:get() == "tmux" then
+    return
+  end
+  local sep = package.config:sub(1, 1)
+  for _, item in ipairs(vim.split(fn.expand "%:p", sep)) do
+    if item:match "tmux" then
+      vim.cmd [[setfiletype tmux]]
+      return
+    end
+  end
+end)
