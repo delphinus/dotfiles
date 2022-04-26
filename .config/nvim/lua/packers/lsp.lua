2b627c596899849cf2a53eaa98e40d857a68bb74
delphinus <me@delphinus.dev>
2022-04-26 11:15:19 +0900

    Set extra paths for iterm2 lib on pyright

--- .config/nvim/lua/packers/lsp.lua
+++ .config/nvim/lua/packers/lsp.lua
@@ -176,6 +176,12 @@ return {
       end
 
       local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+      local home_dir = function(p)
+        return loop.os_homedir() .. (p or "")
+      end
+      local iterm2_dir = function(p)
+        return home_dir "/.config/iterm2/AppSupport/iterm2env-72/versions/3.8.6/lib/" .. (p or "")
+      end
 
       for name, config in pairs {
         clangd = { on_attach = lsp_on_attach() },
@@ -186,13 +192,29 @@ return {
         metals = { on_attach = lsp_on_attach() },
         --jsonls = {on_attach = lsp_on_attach()},
         --perlls = {on_attach = lsp_on_attach()},
-        pyright = { on_attach = lsp_on_attach() },
         solargraph = { on_attach = lsp_on_attach() },
         sourcekit = { on_attach = lsp_on_attach() },
         terraformls = { on_attach = lsp_on_attach() },
         vimls = { on_attach = lsp_on_attach() },
         yamlls = { on_attach = lsp_on_attach() },
         vuels = { on_attach = lsp_on_attach() },
+
+        pyright = {
+          on_attach = lsp_on_attach(),
+          settings = {
+            python = {
+              analysis = {
+                extraPaths = {
+                  home_dir(),
+                  iterm2_dir "python38.zip",
+                  iterm2_dir "python3.8",
+                  iterm2_dir "python3.8/lib-dynload",
+                  iterm2_dir "python3.8/site-packages",
+                },
+              },
+            },
+          },
+        },
 
         denols = {
           on_attach = lsp_on_attach(),
