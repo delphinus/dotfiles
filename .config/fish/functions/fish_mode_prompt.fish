857b6fca12f5407b25124dd52700672d9970ef09
delphinus <me@delphinus.dev>
2022-04-28 16:34:00 +0900

    Fix mode prompt to show in suffix

--- .config/fish/functions/fish_mode_prompt.fish
+++ .config/fish/functions/fish_mode_prompt.fish
@@ -1,25 +1,45 @@
 function fish_mode_prompt --description "Display the mode for the prompt"
-    # Do nothing if not in vi mode
-    if test "$fish_key_bindings" = fish_vi_key_bindings
-        or test "$fish_key_bindings" = fish_hybrid_key_bindings
-        switch $fish_bind_mode
-            case default
-                set_color --bold cyan
-                echo '[N]'
-            case insert
-                set_color --bold white
-                echo '[I]'
-            case replace_one
-                set_color --bold yellow
-                echo '[r]'
-            case replace
-                set_color --bold yellow
-                echo '[R]'
-            case visual
-                set_color --bold green
-                echo '[V]'
-        end
-        set_color normal
-        echo -n ' '
+  set -l last_pipestatus $pipestatus
+  set -l normal (set_color normal)
+
+  # Color the prompt differently when we're root
+  set -l color_cwd $fish_color_cwd
+
+  # for mode prompt
+  set -l suffix
+  if test "$fish_key_bindings" = fish_vi_key_bindings
+    or test "$fish_key_bindings" = fish_hybrid_key_bindings
+    switch $fish_bind_mode
+      case default
+        set suffix (set_color red)❮(set_color yellow)❮(set_color green)❮$normal
+      case insert
+        set suffix (set_color red)❯(set_color yellow)❯(set_color green)❯$normal
+      case replace_one
+        set suffix (set_color yellow)❮❮❮$normal
+      case replace
+        set suffix (set_color magenta)❮❮❮$normal
+      case visual
+        set suffix (set_color green)❮❮❮$normal
     end
+  else
+    set suffix (set_color red)❯(set_color yellow)❯(set_color green)❯$normal
+  end
+
+  if contains -- $USER root toor
+    if set -q fish_color_cwd_root
+      set color_cwd $fish_color_cwd_root
+    end
+    set suffix '#'
+  end
+
+  # If we're running via SSH, change the host color.
+  set -l color_host $fish_color_host
+  if set -q SSH_TTY
+    set color_host $fish_color_host_remote
+  end
+
+  # Write pipestatus
+  set -l prompt_status (__fish_print_pipestatus " [" "]" "|" (set_color $fish_color_status) (set_color --bold $fish_color_status) $last_pipestatus)
+
+  echo -n -s (set_color $color_cwd) (prompt_pwd) $normal (fish_vcs_prompt) $normal $prompt_status " " $suffix " "
 end
