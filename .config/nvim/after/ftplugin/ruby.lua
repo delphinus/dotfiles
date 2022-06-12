80ce0f14fa03796b894f7e9550110fe8a190c757
delphinus <me@delphinus.dev>
2022-06-12 15:59:21 +0900

    Call RailsDetect when available

--- .config/nvim/after/ftplugin/ruby.lua
+++ .config/nvim/after/ftplugin/ruby.lua
@@ -6,7 +6,7 @@ api.create_autocmd("Syntax", {
   group = api.create_augroup("ruby_syntax", {}),
   pattern = "ruby",
   callback = function()
-    if fn.RailsDetect() == 0 then
+    if fn.exists "RailsDetect" == 1 and fn.RailsDetect() == 0 then
       vim.cmd [[
           hi def link rubyRailsTestMethod Function
           syn keyword rubyRailsTestMethod describe context it its specify shared_context shared_examples shared_examples_for shared_context include_examples include_context it_should_behave_like it_behaves_like before after around subject fixtures controller_name helper_name scenario feature background given described_class
