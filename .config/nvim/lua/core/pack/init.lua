c172c183497cffade21c35cf31cbf1a18a657988
delphinus <me@delphinus.dev>
2022-07-30 20:08:47 +0900

    Fix packer commands

--- .config/nvim/lua/core/pack/init.lua
+++ .config/nvim/lua/core/pack/init.lua
@@ -18,7 +18,7 @@ end
 -- }}}
 
 -- commands {{{
-local function run_packer(method, opts)
+local function run_packer(method)
   return function(opts)
     require("core.pack.load")[method](opts)
   end
@@ -39,22 +39,31 @@ api.create_user_command("PackerProfile", run_packer "profile_output", {
 api.create_user_command("PackerStatus", run_packer "status", {
   desc = "[Packer] Output plugins status",
 })
+
 api.create_user_command("PackerSync", function()
   vim.notify "Sync started"
-  run_packer "sync"()
+  run_packer "sync" ()
 end, { desc = "[Packer] Sync plugins" })
+
 api.create_user_command("PackerCompile", function(opts)
-  vim.notify "Compile started"
-  run_packer "compile"(opts.args)
+  api.create_autocmd("User", {
+    pattern = "PackerCompileDone",
+    once = true,
+    callback = function()
+      vim.notify "Compile done"
+    end,
+  })
+  run_packer "compile" (opts.args)
 end, { desc = "[Packer] Compile plugins", nargs = "*" })
+
 api.create_user_command("PackerLoad", function(opts)
   local args = vim.split(opts.args, " ")
   table.insert(args, opts.bang)
-  run_packer "loader"(unpack(opts))
+  run_packer "loader" (unpack(opts))
 end, {
   bang = true,
   complete = function(lead)
-    return run_packer "loader_complete"(lead)
+    return run_packer "loader_complete" (lead)
   end,
   desc = "[Packer] Load plugins",
   nargs = "+",
