e8c08e1d006f56a06eb06f66f7b2f700d719958b
delphinus <me@delphinus.dev>
2022-08-24 22:44:37 +0900

    Use special color for filetype: help

--- .config/nvim/after/ftplugin/help.lua
+++ .config/nvim/after/ftplugin/help.lua
@@ -2,3 +2,11 @@ vim.opt.list = false
 vim.opt.number = false
 vim.opt.relativenumber = false
 vim.b.cursorword = 0
+
+local name = "colorscheme_ft_help"
+local ns = vim.api.nvim_get_namespaces()[name]
+if not ns then
+  ns = vim.api.nvim_create_namespace(name)
+  vim.api.nvim_set_hl(ns, "Comment", { fg = "#c29d53", italic = true })
+end
+vim.api.nvim_win_set_hl_ns(0, ns)
