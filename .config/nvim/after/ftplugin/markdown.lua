f0db2fd214a647a990c778dc96a9396a55fed7f7
delphinus <me@delphinus.dev>
2022-09-20 15:47:22 +0900

    Open markdown files with conceallevel=2

--- .config/nvim/after/ftplugin/markdown.lua
+++ .config/nvim/after/ftplugin/markdown.lua
@@ -5,5 +5,6 @@ vim.keymap.set("n", "<A-M>", "<Plug>StopMarkdownPreview", { buffer = true, remap
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = [[v:lua.require'delphinus.markdown'.foldexpr()]]
 vim.opt.foldtext = [[v:lua.require'delphinus.markdown'.foldtext()]]
+vim.opt.conceallevel = 2
 
 vim.treesitter.start(0, "markdown")
