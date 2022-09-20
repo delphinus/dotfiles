c0b3c94810d2ef8e2654d5d7c078db58ea28af34
delphinus <me@delphinus.dev>
2022-09-20 10:37:05 +0900

    Remove non-working rules

--- .config/nvim/after/queries/markdown/highlights.scm
+++ .config/nvim/after/queries/markdown/highlights.scm
@@ -1,16 +1,11 @@
+;; extends
+
 (atx_heading
   (atx_h1_marker) @text.emphasis @text.underline
-  (heading_content) @text.emphasis @text.underline
+  (inline) @text.emphasis @text.underline
 )
 
-(inline_link) @attribute
-(link_text) @string
-
-(collapsed_reference_link) @attribute
-(collapsed_reference_link (link_text) @attribute @string)
 (link_label) @attribute @string
-(image) @attribute
-(image_description) @string @text.emphasis
 
 ; TODO: This makes Neovim too slow.
 ;(inline_link (link_text) @attribute)
