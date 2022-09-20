9873565b19186e82d724e82f438dc6aadea6b080
delphinus <me@delphinus.dev>
2022-09-20 15:47:38 +0900

    Detect `prompt` filetypes as `bash

--- .config/nvim/after/queries/markdown/injections.scm
+++ .config/nvim/after/queries/markdown/injections.scm
@@ -6,3 +6,10 @@
   (#vim-match? @lang "^(sh|bash)$")
   (#set! language "bash")
 )
+
+(fenced_code_block
+  (info_string) @lang
+  (code_fence_content) @content
+  (#vim-match? @lang "^prompt$")
+  (#set! language "bash")
+)
