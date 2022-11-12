5e7201b082562af7c8892bb5d80651d46127ea28
delphinus <me@delphinus.dev>
2022-11-13 07:40:23 +0900

    Do not store the original notify because unneeded

--- .config/nvim/lua/modules/opt/config.lua
+++ .config/nvim/lua/modules/opt/config.lua
@@ -583,10 +583,8 @@ return {
 
       -- HACK: avoid to set duplicatedly (ex. after PackerCompile)
       --if not _G.__vim_notify_overwritten then
-      local orig = vim.notify
       vim.notify = function(...)
         local args = { ... }
-        vim.notify = orig
         require "notify"
         require "noice"
         vim.schedule(function()
