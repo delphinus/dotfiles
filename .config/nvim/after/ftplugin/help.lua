aa40fc894932ae522ff582f0ef8154c7665cf9c7
delphinus <me@delphinus.dev>
2022-12-04 20:25:04 +0900

    Use the default color for help

--- .config/nvim/after/ftplugin/help.lua
+++ .config/nvim/after/ftplugin/help.lua
@@ -2,11 +2,3 @@ vim.opt.list = false
 vim.opt.number = false
 vim.opt.relativenumber = false
 vim.b.cursorword = 0
-
-local name = "colorscheme_ft_help"
-local ns = api.get_namespaces()[name]
-if not ns then
-  ns = api.create_namespace(name)
-  api.set_hl(ns, "Comment", { fg = "#c29d53", italic = true })
-end
-api.win_set_hl_ns(0, ns)
