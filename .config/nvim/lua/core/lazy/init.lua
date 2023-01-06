499d1462010dd05625c2556b9ffb4f9e57e82ec8
delphinus <me@delphinus.dev>
2023-01-06 12:14:15 +0900

    Ignore loading ftdetect scripts and use mine

--- .config/nvim/lua/core/lazy/init.lua
+++ .config/nvim/lua/core/lazy/init.lua
@@ -1,6 +1,7 @@
 local fn, uv = require("core.utils").globals()
 
-local lazy_path = fn.stdpath "data" .. "/lazy/lazy.nvim"
+local pack_path = fn.stdpath "data" .. "/lazy/"
+local lazy_path = pack_path .. "lazy.nvim"
 if not uv.fs_stat(lazy_path) then
   fn.system { "git", "clone", "--filter=blob:none", "--single-branch", "https://github.com/folke/lazy.nvim", lazy_path }
 end
@@ -10,6 +11,31 @@ local plugins = {}
 for _, name in ipairs { "start", "opt", "cmp", "lsp", "telescope" } do
   table.insert(plugins, require("lazies." .. name))
 end
+
+local ignore_ftdetect = {}
+for _, i in ipairs {
+  "Vim-Jinja2-Syntax",
+  "ansible-vim",
+  "bats.vim",
+  "ejs-syntax",
+  "moonscript-vim",
+  "plantuml-syntax",
+  "skkdict.vim",
+  "vader.vim",
+  "vifm.vim",
+  "vim-caddyfile",
+  "vim-coffee-script",
+  "vim-cpanfile",
+  "vim-firestore",
+  "vim-fish",
+  "vim-mustache-handlebars",
+  "vim-perl",
+  "xslate-vim",
+} do
+  ignore_ftdetect[pack_path .. i] = true
+end
+
+require("lazy.core.loader").did_ftdetect = ignore_ftdetect
 
 require("lazy").setup(plugins, {
   defaults = { lazy = true },
