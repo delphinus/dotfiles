d422f40a5ce98470e23b5f11111943929d59eede
delphinus <me@delphinus.dev>
2023-02-23 19:09:15 +0900

    Fix mappings

--- .config/nvim/lua/lazies/cmp.lua
+++ .config/nvim/lua/lazies/cmp.lua
@@ -17,8 +17,8 @@ return {
     "vim-skk/skkeleton",
     keys = {
       -- Use these mappings in Karabiner-Elements
-      { "<F10>", "<Plug>(skkeleton-enable)", mode = { "i", "c", "l" } },
-      { "<F13>", "<Plug>(skkeleton-disable)", mode = { "i", "c", "l" } },
+      { "<F10>", "<Plug>(skkeleton-disable)", mode = { "i", "c", "l" } },
+      { "<F13>", "<Plug>(skkeleton-enable)", mode = { "i", "c", "l" } },
       { "<C-j>", "<Plug>(skkeleton-toggle)", mode = { "i", "c", "l" } },
       { "<C-x><C-o>", lazy_require("cmp").complete(), mode = { "i" } },
     },
