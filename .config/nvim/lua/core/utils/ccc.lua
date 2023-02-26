2c87826171d4397767e935e8db681aac4a6cff01
delphinus <me@delphinus.dev>
2023-02-26 18:24:16 +0900

    Fix to detect multi names in each line

--- .config/nvim/lua/core/utils/ccc.lua
+++ .config/nvim/lua/core/utils/ccc.lua
@@ -2,23 +2,32 @@
 ---@return table
 return function(color_table)
   local parse = require "ccc.utils.parse"
+  local names = {}
   local converted = {}
   for name, rgb in pairs(color_table) do
     local r, g, b = rgb:match "(%x%x)(%x%x)(%x%x)"
     if r then
+      table.insert(names, name)
       converted[name] = { parse.hex(r), parse.hex(g), parse.hex(b) }
     end
   end
+  table.sort(names, function(a, b)
+    return #a > #b
+  end)
+  local min = #names[#names]
+  local re = vim.regex([[\<]] .. table.concat(names, [[\|]]) .. [[\>]])
   return {
-    ---@param _ any
-    ---@param s string
-    ---@param init integer
     parse_color = function(_, s, init)
-      for name, parsed in pairs(converted) do
-        local start, end_ = s:find(name, init)
-        if start then
-          return start, end_, parsed
-        end
+      init = vim.F.if_nil(init, 1)
+      local target = s:sub(init)
+      if #target < min then
+        return
+      end
+      local start, end_ = re:match_str(target)
+      if start then
+        local name = target:sub(start + 1, end_)
+        --vim.notify(vim.inspect({ start, end_, name }, { newline = "", indent = "" }))
+        return start + init, end_ + init - 1, converted[name]
       end
     end,
   }
