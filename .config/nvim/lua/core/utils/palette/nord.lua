db998804c86279f1257dd1181008149f745063eb
delphinus <me@delphinus.dev>
2023-03-21 16:55:44 +0900

    Separate table from logic

---
+++ .config/nvim/lua/core/utils/palette/nord.lua
@@ -0,0 +1,55 @@
+local palette = {
+  -- default colors from nord
+  nord0 = "#2E3440",
+  nord1 = "#3B4252",
+  nord2 = "#434C5E",
+  nord3 = "#4C566A",
+  nord3_bright = "#616E88",
+  nord4 = "#D8DEE9",
+  nord5 = "#E5E9F0",
+  nord6 = "#ECEFF4",
+  nord7 = "#8FBCBB",
+  nord8 = "#88C0D0",
+  nord9 = "#81A1C1",
+  nord10 = "#5E81AC",
+  nord11 = "#BF616A",
+  nord12 = "#D08770",
+  nord13 = "#EBCB8B",
+  nord14 = "#A3BE8C",
+  nord15 = "#B48EAD",
+
+  black = "#3B4252",
+  red = "#BF616A",
+  green = "#A3BE8C",
+  yellow = "#EBCB8B",
+  blue = "#81A1C1",
+  magenta = "#B48EAD",
+  cyan = "#88C0D0",
+  white = "#E5E9F0",
+  bright_black = "#4C566A",
+  bright_red = "#BF616A",
+  bright_green = "#A3BE8C",
+  bright_yellow = "#EBCB8B",
+  bright_blue = "#81A1C1",
+  bright_magenta = "#B48EAD",
+  bright_cyan = "#8FBCBB",
+  bright_white = "#ECEFF4",
+}
+
+palette.orange = palette.nord12
+palette.brighter_red = "#E5989F"
+palette.brighter_blue = "#8CA9CD"
+palette.brighter_black = palette.nord3_bright
+palette.dark_black = palette.nord0
+palette.dark_white = palette.nord4
+palette.dark_blue = palette.nord10
+palette.bg_green = "#183203"
+palette.bg_yellow = "#432d00"
+palette.bg_red = "#52050c"
+palette.gray = palette.nord3
+
+palette.comment = "#72809a"
+palette.context = "#365F86"
+palette.border = "#5D9794"
+
+return palette
