7f737a063b524cff50118ee0ed0d13dacb515541
delphinus <me@delphinus.dev>
2023-08-14 07:39:16 +0900

    Deal with breaking changes in treesitter

--- .config/nvim/after/queries/markdown/injections.scm
+++ .config/nvim/after/queries/markdown/injections.scm
@@ -2,28 +2,28 @@
 
 (fenced_code_block
   (info_string) @lang
-  (code_fence_content) @content
+  (code_fence_content) @injection.content
   (#vim-match? @lang "^(sh|bash|shell)$")
-  (#set! language "bash")
+  (#set! injection.language "bash")
 )
 
 (fenced_code_block
   (info_string) @lang
-  (code_fence_content) @content
+  (code_fence_content) @injection.content
   (#vim-match? @lang "^prompt$")
-  (#set! language "bash")
+  (#set! injection.language "bash")
 )
 
 (fenced_code_block
   (info_string) @lang
-  (code_fence_content) @content
+  (code_fence_content) @injection.content
   (#vim-match? @lang "^(vim)(:.*)?$")
-  (#set! language vim)
+  (#set! injection.language vim)
 )
 
 (fenced_code_block
   (info_string) @lang
-  (code_fence_content) @content
+  (code_fence_content) @injection.content
   (#vim-match? @lang "^(lua)(:.*)?$")
-  (#set! language lua)
+  (#set! injection.language lua)
 )
