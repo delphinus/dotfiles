8c85a81797c60fdc4c7638586f9383a239bcddf1
delphinus <me@delphinus.dev>
2024-01-30 14:13:04 +0900

    Remove unnecessary config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -22,7 +22,6 @@ config.quick_select_patterns = { const.url_regex, const.hash_regex, const.path_r
 config.set_environment_variables = { SHELL = const.fish_bin }
 -- config.use_fancy_tab_bar = false
 config.warn_about_missing_glyphs = false
-config.webgpu_power_preference = "HighPerformance"
 config.window_background_opacity = 0.96
 config.window_close_confirmation = "NeverPrompt"
 config.window_decorations = "RESIZE"
