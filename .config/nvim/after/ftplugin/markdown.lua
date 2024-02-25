c8ed2ce6cb650eecd139f906eba136afaf323e0e
delphinus <me@delphinus.dev>
2024-02-25 15:12:00 +0900

    Fix fold

--- .config/nvim/after/ftplugin/markdown.lua
+++ .config/nvim/after/ftplugin/markdown.lua
@@ -2,9 +2,9 @@
 vim.keymap.set("n", "<A-m>", "<Plug>MarkdownPreview", { buffer = true, remap = true })
 vim.keymap.set("n", "<A-M>", "<Plug>StopMarkdownPreview", { buffer = true, remap = true })
 
-vim.opt.foldmethod = "expr"
-vim.opt.foldexpr = [[v:lua.require'delphinus.markdown'.foldexpr()]]
-vim.opt.foldtext = [[v:lua.require'delphinus.markdown'.foldtext()]]
-vim.opt.conceallevel = 2
+vim.opt_local.foldmethod = "expr"
+vim.opt_local.foldexpr = [[v:lua.require'delphinus.markdown'.foldexpr()]]
+vim.opt_local.foldtext = [[v:lua.require'delphinus.markdown'.foldtext()]]
+vim.opt_local.conceallevel = 2
 
 vim.treesitter.start(0, "markdown")
