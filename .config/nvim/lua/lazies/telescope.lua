f89a98b5ac9068adb7d0eb03d7a519ab7ecf7c04
delphinus <me@delphinus.dev>
2024-03-29 17:59:28 +0900

    Add trouble.nvim

--- .config/nvim/lua/lazies/telescope.lua
+++ .config/nvim/lua/lazies/telescope.lua
@@ -111,6 +111,7 @@ return {
       { "nvim-telescope/telescope-smart-history.nvim", dependencies = { "kkharji/sqlite.lua" } },
       { "nvim-telescope/telescope-symbols.nvim" },
       { "nvim-telescope/telescope-z.nvim" },
+
       { "jonarrien/telescope-cmdline.nvim" },
       { "stevearc/dressing.nvim" },
       {
@@ -551,5 +552,19 @@ return {
         "<Cmd>Octo search reviewed-by:@me review:approved is:open is:pr archived:false<CR>"
       )
     end,
+  },
+
+  {
+    "folke/trouble.nvim",
+    branch = "dev",
+    cmd = { "Trouble" },
+    keys = {
+      { "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", desc = "Diagnostics (Trouble)" },
+      { "<leader>xX", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "Buffer Diagnostics (Trouble)" },
+      { "<leader>cs", "<cmd>Trouble symbols toggle focus=false<cr>", desc = "Symbols (Trouble)" },
+      { "<leader>xL", "<cmd>Trouble loclist toggle<cr>", desc = "Location List (Trouble)" },
+      { "<leader>xQ", "<cmd>Trouble qflist toggle<cr>", desc = "Quickfix List (Trouble)" },
+    },
+    opts = {},
   },
 }
