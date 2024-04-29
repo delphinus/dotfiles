7c4d8c4ec5c45747b7d872b01293e69efa93c330
delphinus <me@delphinus.dev>
2024-04-29 13:00:17 +0900

    Use the master of telescope-frecency

--- .config/nvim/lua/lazies/telescope.lua
+++ .config/nvim/lua/lazies/telescope.lua
@@ -104,7 +104,7 @@ return {
       },
 
       { "nvim-telescope/telescope-file-browser.nvim" },
-      { "nvim-telescope/telescope-frecency.nvim", branch = "feat/fuzzy-match" },
+      { "nvim-telescope/telescope-frecency.nvim" },
       { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
       { "nvim-telescope/telescope-ghq.nvim" },
       { "nvim-telescope/telescope-github.nvim" },
