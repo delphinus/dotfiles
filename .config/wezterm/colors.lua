fe599df7abd8e2493381c5e6403d5a5632b0ecb7
delphinus <me@delphinus.dev>
2024-06-24 15:23:00 +0900

    Use sweetie in wezterm

--- .config/wezterm/colors.lua
+++ .config/wezterm/colors.lua
@@ -1,20 +1,14 @@
 local wezterm = require "wezterm"
 
 return function(config)
-  config.color_scheme = "nord"
-  config.colors = {
-    tab_bar = {
-      active_tab = { bg_color = "#88c0d0", fg_color = "#2e3440" },
-      inactive_tab = { bg_color = "#434c5e", fg_color = "#d8dee9" },
-      inactive_tab_hover = { bg_color = "#4C566A", fg_color = "#d8dee9" },
-      new_tab = { bg_color = "#4c566a", fg_color = "#d8dee9" },
-      new_tab_hover = { bg_color = "#4c566a", fg_color = "#d8dee9" },
-    },
-  }
+  local sweetie = "/Users/jinnouchi.yasushi/.local/share/nvim/lazy/sweetie.nvim"
+  if #wezterm.glob(sweetie) > 0 then
+    config.colors = wezterm.color.load_scheme(sweetie .. "/extras/wezterm/sweetie_dark.toml")
+  else
+    config.color_scheme = "nord"
+  end
   config.window_frame = {
     font = wezterm.font { family = "SF Mono Square", weight = "Bold" },
     font_size = 16.0,
-    active_titlebar_bg = "#2e3440",
-    inactive_titlebar_bg = "#2e3440",
   }
 end
