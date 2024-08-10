54c6a81468d323beb5f8cdc98b1a5d2fe84473f5
delphinus <me@delphinus.dev>
2024-08-10 11:00:24 +0900

    Add quicker.nvim

--- .config/nvim/lua/lazies/opt.lua
+++ .config/nvim/lua/lazies/opt.lua
@@ -1206,4 +1206,45 @@ return {
       end,
     },
   },
+
+  {
+    "stevearc/quicker.nvim",
+    ft = { "qf" },
+    keys = {
+      {
+        "<leader>q",
+        function()
+          require("quicker").toggle()
+        end,
+        mode = "n",
+        desc = "Toggle quickfix",
+      },
+      {
+        "<leader>l",
+        function()
+          require("quicker").toggle { loclist = true }
+        end,
+        mode = "n",
+        desc = "Toggle loclist",
+      },
+    },
+    opts = {
+      keys = {
+        {
+          ">",
+          function()
+            require("quicker").expand { before = 2, after = 2, add_to_existing = true }
+          end,
+          desc = "Expand quickfix context",
+        },
+        {
+          "<",
+          function()
+            require("quicker").collapse()
+          end,
+          desc = "Collapse quickfix context",
+        },
+      },
+    },
+  },
 }
