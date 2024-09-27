2b01032530d8ae23c67f4b0a61ac27f7c0d9bb8c
delphinus <me@delphinus.dev>
2024-09-27 15:29:05 +0900

    Arrange comparators for cmp

--- .config/nvim/lua/lazies/cmp.lua
+++ .config/nvim/lua/lazies/cmp.lua
@@ -454,12 +454,12 @@ return {
             require("copilot_cmp.comparators").prioritize,
 
             -- Below is the default comparitor list and order for nvim-cmp
+            cmp.config.compare.recently_used,
+            cmp.config.compare.scopes,
+            cmp.config.compare.locality,
             cmp.config.compare.offset,
-            -- cmp.config.compare.scopes, --this is commented in nvim-cmp too
             cmp.config.compare.exact,
             cmp.config.compare.score,
-            cmp.config.compare.recently_used,
-            cmp.config.compare.locality,
             cmp.config.compare.kind,
             cmp.config.compare.sort_text,
             cmp.config.compare.length,
