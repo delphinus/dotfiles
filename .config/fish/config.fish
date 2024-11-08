5dc7fa1d5c0bb9781709c7fc75ed6ba4008d98c8
delphinus <me@delphinus.dev>
2024-11-08 11:36:49 +0900

    Use custom version of `done`

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -206,3 +206,19 @@ else
     set -x EXA_ICON_SPACING 2
 end
 alias l 'eza -lF --group-directories-first --color-scale --icons --time-style long-iso --git'
+
+# https://zenn.dev/ryoppippi/articles/de6c931cc1028f
+set -gx NOTIFY_ON_COMMAND_DURATION 5000
+function fish_right_prompt
+    if test $CMD_DURATION
+        # Show notification if dration is more than 30 seconds
+        if test $CMD_DURATION -gt $NOTIFY_ON_COMMAND_DURATION
+            # Show duration of the last command in seconds
+            set duration (echo "$CMD_DURATION 1000" | awk '{printf "%.3fs", $1 / $2}')
+            echo $duration
+            set -f MSG (echo (history | head -1) returned $status after $duration)
+            type -q notify-send && notify-send $MSG && return
+            type -q terminal-notifier && terminal-notifier -message $MSG && return
+        end
+    end
+end
