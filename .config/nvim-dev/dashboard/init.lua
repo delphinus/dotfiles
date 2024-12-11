f263bf3431b7d56267fd2889ab8152985476e492
delphinus <me@delphinus.dev>
2024-12-11 17:02:35 +0900

    Update dashboard config

--- .config/nvim-dev/dashboard/init.lua
+++ .config/nvim-dev/dashboard/init.lua
@@ -1,19 +1,4 @@
--- Bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system { "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath }
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
-end
-vim.opt.rtp:prepend(lazypath)
+load(vim.fn.system "curl -s https://raw.githubusercontent.com/folke/lazy.nvim/main/bootstrap.lua")()
 
 require("lazy").setup {
   { "nvim-tree/nvim-web-devicons", lazy = true },
