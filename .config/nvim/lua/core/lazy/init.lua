1959d090e5ab14e16834e3cec743956e0ff96187
delphinus <me@delphinus.dev>
2024-12-16 16:30:20 +0900

    Add method to measure true time

--- .config/nvim/lua/core/lazy/init.lua
+++ .config/nvim/lua/core/lazy/init.lua
@@ -6,25 +6,39 @@ if vim.env.PROF then
   }
 end
 
-_G.pp = function(v)
-  vim.notify(vim.inspect(v, { newline = "", indent = "" }))
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+if vim.uv.fs_stat(lazypath) then
+  vim.opt.rtp:prepend(lazypath)
+else
+  load(vim.fn.system "curl -s https://raw.githubusercontent.com/folke/lazy.nvim/main/bootstrap.lua")()
 end
 
-local fn, uv = require("core.utils").globals()
+do
+  local stats = require "lazy.stats"
+  stats.cputime() -- HACK: this is needed to use clock_gettime()
+  stats.monotonic = function()
+    local ffi = require "ffi"
+    local pnano = assert(ffi.new("nanotime[?]", 1))
+    local CLOCK_MONOTONIC = jit.os == "OSX" and 6 or 1
+    ffi.C.clock_gettime(CLOCK_MONOTONIC, pnano)
+    return tonumber(pnano[0].tv_sec) * 1e3 + tonumber(pnano[0].tv_nsec) / 1e6
+  end
 
-local pack_path = fn.stdpath "data" .. "/lazy/"
-local lazy_path = pack_path .. "lazy.nvim"
-if not uv.fs_stat(lazy_path) then
-  fn.system {
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "--single-branch",
-    "https://github.com/folke/lazy.nvim.git",
-    lazy_path,
-  }
+  if vim.env.LOGFILE then
+    local start = stats.monotonic()
+    vim.api.nvim_create_autocmd("User", {
+      once = true,
+      pattern = "DashboardLoaded",
+      callback = function()
+        local finish = stats.monotonic()
+        vim.fn.writefile({ tostring(finish - start) }, vim.env.LOGFILE, "a")
+        vim.schedule(function()
+          vim.cmd.qall { bang = true }
+        end)
+      end,
+    })
+  end
 end
-vim.opt.runtimepath:prepend(lazy_path)
 
 local use_minimal = not not vim.env.MINIMAL or vim.env.USER == "root"
 local definitions = use_minimal and { "minimal", "cmp" } or { "minimal", "start", "opt", "cmp", "lsp", "telescope" }
@@ -53,7 +67,7 @@ for _, i in ipairs {
   "vim-perl",
   "xslate-vim",
 } do
-  ignore_ftdetect[pack_path .. i] = true
+  ignore_ftdetect[lazypath .. "/" .. i] = true
 end
 
 vim.api.nvim_create_autocmd("User", {
