b8143285d8fab8fc5b605fdb75f380c838d78b0a
delphinus <me@delphinus.dev>
2024-12-16 14:29:06 +0900

    Add command to test semver in lazy.nvim

--- .config/nvim/lua/core/options/commands.lua
+++ .config/nvim/lua/core/options/commands.lua
@@ -95,3 +95,13 @@ vim.cmd.nnoremenu [[PopUp.Toggle\ Diagnostic <Cmd>lua vim.diagnostic[vim.diagnos
 vim.cmd.nnoremenu [[PopUp.Toggle\ Auto\ Formatting <Cmd>lua require("auto_fmt").toggle()<CR>]]
 
 vim.cmd.cabbrev("IN", "Inspect")
+
+vim.api.nvim_create_user_command("LazySemver", function(opts)
+  if #opts.fargs ~= 2 then
+    vim.notify("needs 2 args: spec and version", vim.log.levels.ERROR)
+    return
+  end
+  local sv = require "lazy.manage.semver"
+  local matched = sv.range(opts.fargs[1]):matches(opts.fargs[2])
+  vim.notify(matched and "OK" or "NG", vim.log.levels[matched and "info" or "WARN"])
+end, { nargs = "*" })
