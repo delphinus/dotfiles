e835bd84370c71e94d0f7f6fdd3baa24f11b10a5
delphinus <me@delphinus.dev>
2025-01-05 15:23:01 +0900

    Add alternative code for vim-lastplace

--- .config/nvim/lua/core/options/set.lua
+++ .config/nvim/lua/core/options/set.lua
@@ -227,4 +227,27 @@ end
 -- NOTE: for man.lua (:Man)
 vim.g.man_hardwrap = 0
 
+-- vim-lastplace alternative
+-- https://github.com/neovim/neovim/issues/16339#issuecomment-1457394370
+vim.api.nvim_create_autocmd("BufRead", {
+  callback = function(opts)
+    vim.api.nvim_create_autocmd("BufWinEnter", {
+      once = true,
+      buffer = opts.buf,
+      callback = function()
+        local ft = vim.bo[opts.buf].filetype
+        local last_known_line = vim.api.nvim_buf_get_mark(opts.buf, '"')[1]
+        if
+          not ft:match "commit"
+          and not ft:match "rebase"
+          and last_known_line > 1
+          and last_known_line <= vim.api.nvim_buf_line_count(opts.buf)
+        then
+          vim.api.nvim_feedkeys([[g`"]], "nx", false)
+        end
+      end,
+    })
+  end,
+})
+
 -- vim:se fdm=marker:
