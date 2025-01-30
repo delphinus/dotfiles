790117ba02b8b4f10299a82878feba5d06043380
delphinus <me@delphinus.dev>
2025-01-30 12:09:11 +0900

    Add VGit

--- .config/nvim/lua/lazies/opt.lua
+++ .config/nvim/lua/lazies/opt.lua
@@ -1352,4 +1352,60 @@ return {
       },
     },
   },
+
+  {
+    "tanvirtin/vgit.nvim",
+    cmd = "VGit",
+    version = "*",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {
+      keymaps = {
+        ["n <leader>gk"] = function()
+          require("vgit").hunk_up()
+        end,
+        ["n <leader>gj"] = function()
+          require("vgit").hunk_down()
+        end,
+        ["n <leader>gs"] = function()
+          require("vgit").buffer_hunk_stage()
+        end,
+        ["n <leader>gr"] = function()
+          require("vgit").buffer_hunk_reset()
+        end,
+        ["n <leader>gp"] = function()
+          require("vgit").buffer_hunk_preview()
+        end,
+        ["n <leader>gb"] = function()
+          require("vgit").buffer_blame_preview()
+        end,
+        ["n <leader>gf"] = function()
+          require("vgit").buffer_diff_preview()
+        end,
+        ["n <leader>gh"] = function()
+          require("vgit").buffer_history_preview()
+        end,
+        ["n <leader>gu"] = function()
+          require("vgit").buffer_reset()
+        end,
+        ["n <leader>gg"] = function()
+          require("vgit").buffer_gutter_blame_preview()
+        end,
+        ["n <leader>glu"] = function()
+          require("vgit").buffer_hunks_preview()
+        end,
+        ["n <leader>gls"] = function()
+          require("vgit").project_hunks_staged_preview()
+        end,
+        ["n <leader>gd"] = function()
+          require("vgit").project_diff_preview()
+        end,
+        ["n <leader>gq"] = function()
+          require("vgit").project_hunks_qf()
+        end,
+        ["n <leader>gx"] = function()
+          require("vgit").toggle_diff_preference()
+        end,
+      },
+    },
+  },
 }
