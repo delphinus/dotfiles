be054326689626fca15d4631bf6b22bcbe3a9fe4
delphinus <me@delphinus.dev>
2025-05-19 13:41:51 +0900

    Change perl LS settings in accordance w/ file

--- .config/nvim/after/ftplugin/perl.lua
+++ .config/nvim/after/ftplugin/perl.lua
@@ -45,3 +45,29 @@ if not found then
 end
 
 vim.cmd.setlocal("foldtext=" .. perl_fold_text:vim() .. "()")
+
+-- HACK: Command to launch `perl` should be changed in accordance with its root dir
+do
+  local lines = vim.fn.readfile(vim.uv.os_homedir() .. "/.1password-perl-env")
+  local env = vim.iter(lines):fold({}, function(a, b)
+    local key, value = b:match "^([^=]+)=(.*)$"
+    if key and value then
+      a[key] = value
+    end
+    return a
+  end)
+
+  local path = vim.api.nvim_buf_get_name(0)
+  local use_carmel = 0 < #vim.fs.find(".carmel/MySetup.pm", { upward = true, type = "file", path = path })
+
+  vim.lsp.config("perlnavigator", {
+    settings = {
+      perlnavigator = {
+        perlEnv = env,
+        perlPath = use_carmel and "carmel" or "perl",
+        perlParams = use_carmel and { "exec", "perl" } or {},
+        includePaths = { "./lib", "./local/lib/perl5", "./t/lib" },
+      },
+    },
+  })
+end
