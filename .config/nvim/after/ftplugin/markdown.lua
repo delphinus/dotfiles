59022e76323556adc466a3a22a27bbf78e7daddd
delphinus <me@delphinus.dev>
2025-07-31 20:30:37 +0900

    Disable conceal in Markdown

--- .config/nvim/after/ftplugin/markdown.lua
+++ .config/nvim/after/ftplugin/markdown.lua
@@ -5,7 +5,7 @@ vim.keymap.set("n", "<A-M>", "<Plug>StopMarkdownPreview", { buffer = true, remap
 vim.opt_local.foldmethod = "expr"
 vim.opt_local.foldexpr = [[v:lua.require'delphinus.markdown'.foldexpr()]]
 vim.opt_local.foldtext = [[v:lua.require'delphinus.markdown'.foldtext()]]
-vim.opt_local.conceallevel = 3
+vim.opt_local.conceallevel = 0
 vim.opt_local.concealcursor = "nc"
 vim.opt_local.wrap = true
 
