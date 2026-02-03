9304a443f4f2c039c20ba26b002373f856760e13
delphinus <me@delphinus.dev>
2026-02-03 09:37:08 +0900

    Update config for telescope testing

--- .config/nvim-dev/telescope/init.lua
+++ .config/nvim-dev/telescope/init.lua
@@ -8,15 +8,26 @@ end
 require("lazy").setup {
   { "nvim-telescope/telescope-frecency.nvim", opts = {} },
   {
-    "nvim-telescope/telescope.nvim",
+    -- "nvim-telescope/telescope.nvim",
+    "delphinus/telescope.nvim",
+    branch = "feat/cache-man-pages",
     dependencies = { "nvim-lua/plenary.nvim" },
     opts = {},
     keys = {
       {
         "<leader>ff",
-        mode = { "n" },
         ':Telescope frecency workspace=CWD path_display={"smart"} <CR>',
         {
+          mode = { "n" },
+          desc = "Telescope find files using frecency",
+          silent = true,
+        },
+      },
+      {
+        "<leader>fm",
+        ":Telescope man_pages<CR>",
+        {
+          mode = { "n" },
           desc = "Telescope find files using frecency",
           silent = true,
         },
