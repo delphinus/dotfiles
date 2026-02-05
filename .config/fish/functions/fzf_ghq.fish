4514f9afe2ca35320308b3f6d79ef2a7bbcfdc8d
delphinus <me@delphinus.dev>
2026-02-05 12:22:33 +0900

    Use faster-ghq-list for fzf_ghq

--- .config/fish/functions/fzf_ghq.fish
+++ .config/fish/functions/fzf_ghq.fish
@@ -1,20 +1,15 @@
 function fzf_ghq --description 'Move with FZF + ghq'
-  set -l options 'i/insert'
-  argparse $options -- $argv
+    ~/git/dotfiles/bin/faster-ghq-list \
+        | eval "fzf $FZF_DEFAULT_OPTS" \
+        | read -l select
 
-  ghq list --full-path | \
-    perl -pe 's,$ENV{HOME},~,' | \
-    eval "fzf $FZF_DEFAULT_OPTS" | \
-    perl -pe 's/^~(\w*)/(getpwnam($1 || $ENV{USER}))[7]/e' | \
-    read -l select
-
-  if test -n "$select"
-    if set -q _flag_insert
-      commandline -i "$select"
-    else
-      cd "$select"
-      emit fish_prompt
+    if test -n "$select"
+        if set -q _flag_insert
+            commandline -i "$select"
+        else
+            cd "$select"
+            emit fish_prompt
+        end
     end
-  end
-  commandline -f repaint
+    commandline -f repaint
 end
