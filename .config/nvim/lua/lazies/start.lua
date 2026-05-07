7e90dd617ca75224574e831ff0332bb4b0fb94f2
delphinus <me@delphinus.dev>
2026-05-07 12:06:01 +0900

    Add line-justice.nvim

--- .config/nvim/lua/lazies/start.lua
+++ .config/nvim/lua/lazies/start.lua
@@ -343,4 +343,25 @@ return {
       }
     end,
   },
+
+  non_lazy {
+    "zaakiy/line-justice.nvim",
+    dependencies = { "luukvbaal/statuscol.nvim" },
+    config = function()
+      local lj = require "line-justice"
+      lj.setup()
+
+      local builtin = require "statuscol.builtin"
+      require("statuscol").setup {
+        relculright = true,
+        segments = {
+          { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
+          { sign = { namespace = { "gitsigns" }, maxwidth = 1, colwidth = 1, auto = true }, click = "v:lua.ScSa" },
+          { sign = { namespace = { "diagnostic/signs" }, maxwidth = 2, auto = true }, click = "v:lua.ScSa" },
+          { sign = { name = { ".*" }, maxwidth = 2, colwidth = 1, auto = true, wrap = true }, click = "v:lua.ScSa" },
+          { text = { lj.segment }, click = "v:lua.ScLa" },
+        },
+      }
+    end,
+  },
 }
